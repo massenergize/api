7d02f171ed1bbe5c44fee6b8a48a9608f2353361
Samuel Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-04-14 11:30:29 -0400

    adding code to register models

--- src/database/admin.py
+++ src/database/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from massenergize_portal_backend.utils.utils import get_all_registered_models
+import database.models as module
 
 # Register your models here.
+ALL_DB_MODELS = get_all_registered_models(module)
+
+for model in ALL_DB_MODELS:
+  admin.site.register(model)
--- src/massenergize_portal_backend/utils/utils.py
+++ src/massenergize_portal_backend/utils/utils.py
@@ -1,4 +1,7 @@
 import json
+import django.db.models.base as Base
+import inspect
+
 
 def load_json(path):
   """
@@ -10,3 +13,12 @@ def load_json(path):
   with open (path) as file:
     return json.load(file)
   return {}
+
+def get_all_registered_models(module):
+  """
+  Takes a module containing django database models and returns a list of all of
+  them
+  """
+  all_models = [m[1] for m in inspect.getmembers(module, inspect.isclass)
+    if isinstance(m[1], Base.ModelBase)]
+  return all_models
