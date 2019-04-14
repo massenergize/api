e30bba8819bfbb53b8f07c7c952274220aabd424
Samuel Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-04-13 23:11:20 -0400

    defining Permissions Model

--- src/database/models.py
+++ src/database/models.py
@@ -38,8 +38,51 @@ class Event(models.Model):
 
 
 class Permission(models.Model):
-  pass
+  """
+  Represents the Permissions that are required by users to perform any tasks 
+  on this platform.
+  """
+  PERMISSION_TYPES = {
+    'A': 'Approve', 
+    'C': 'Create', 
+    'E': 'Edit',
+    'F': 'Fork',
+    'V': 'View'
+  }
 
+  permission_type = models.CharField(
+    choices=list(TYPES.items()), 
+    primary_key=True
+  ) 
+
+  def can_approve(self):
+    return self.permission_type == 'A';
+
+
+  def can_create(self):
+    return self.permission_type == 'C';
+
+  
+  def can_edit(self):
+    return self.permission_type == 'E';
+
+
+  def can_fork(self):
+    return self.permission_type == 'F';
+
+
+  def can_view(self):
+    return self.permission_type == 'V';
+
+
+  def __str__(self):
+    return 'Can: %s' % TYPES[self.permission_type] 
+
+  class Meta:
+    ordering = ('permtype',)
+    db_table = "permissions_for_users"
+
+    
 
 class Role(models.Model):
   pass 
