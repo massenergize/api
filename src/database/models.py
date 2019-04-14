7f72809a00b5b7e78658c58b40cc146711d7e53b
Samuel Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-04-13 22:42:20 -0400

    adding models skeletons

--- src/database/models.py
+++ src/database/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+class RealEstateUnit(models.Model):
+  pass 
+
+
+class Team(models.Model):
+  pass 
+
+
+class Goal(models.Model):
+  pass 
+
+
+class Neighbourhood(models.Model):
+  pass 
+
+
+class Partner(models.Model):
+  pass 
+
+
+class Action(models.Model):
+  pass 
+
+
+class Tag(models.Model):
+  pass 
+
+
+class Items(models.Model):
+  pass 
+
+
+class Event(models.Model):
+  pass 
+
+
+class Permission(models.Model):
+  pass
+
+
+class Role(models.Model):
+  pass 
+
+
+class Policy(models.Model):
+  pass 
+
+
+class Notification(models.Model):
+  pass
