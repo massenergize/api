8129a7b95db3d489c996c96c7cd909754cd9a8ff
Samuel Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-06-19 13:15:56 -0400

    setting up welcome page for api webste

--- src/_main_/settings.py
+++ src/_main_/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'carbon_calculator',
     'database',
     'user_portal',
+    'website',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
