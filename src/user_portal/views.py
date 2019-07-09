f2ceed150aa4d7c074a7032d91e3234370195852
Samuel Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-07-09 12:27:27 -0400

    cleaning up portal views

--- src/user_portal/views.py
+++ src/user_portal/views.py
@@ -8,11 +8,8 @@ def home(request):
     pageData = fetch.community_portal_home_page_data()
     menuData = fetch.community_portal_website_menu()
     userData = fetch.community_portal_user_data()
-    pageData = read.community_portal_home_page_data()
-    eventsData = read.community_portal_events_page_data()
-    impactData = read.community_portal_impact_data()
-    menuData = read.community_portal_website_menu()
-    userData = read.community_portal_user_data()
+    eventsData = fetch.community_portal_events_page_data()
+    impactData = fetch.community_portal_impact_data()
     return JsonResponse({
         "pageData": pageData,
         "eventsData": eventsData,
@@ -45,13 +42,12 @@ def aboutUs(request):
     menuData = fetch.community_portal_website_menu()
     userData = fetch.community_portal_user_data()
     aboutUsData = fetch.community_portal_about_us_page_data()
-    menuData = read.community_portal_website_menu()
-    userData = read.community_portal_user_data()
-    pageData = read.community_portal_about_us_page_data()
+    pageData = fetch.community_portal_about_us_page_data()
     return JsonResponse({
         "menuData": menuData,
         "userData": userData,
-        "pageData": aboutUsData
+        "pageData": aboutUsData,
+        "aboutUsData": aboutUsData
     })
 
 def events(request):
@@ -65,16 +61,17 @@ def events(request):
         }
     )
 
-def create_user(request):
-    pass
 
 def stories(request):
-    pageData = read.community_portal_stories_page_data()
-    menuData = read.community_portal_website_menu()
-    userData = read.community_portal_user_data()
+    pageData = fetch.community_portal_stories_page_data()
+    menuData = fetch.community_portal_website_menu()
+    userData = fetch.community_portal_user_data()
     return JsonResponse({
         "pageData": pageData, 
         "menuData": menuData,
         "userData": userData,
         }
     )
+
+def create_user(request):
+    pass
