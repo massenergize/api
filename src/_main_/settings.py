65bfdce4934afb801318658dbfa406bffddcdf7d
Mingle Li <limingle5@gmail.com>
2019-07-10 14:55:40 -0400

    Changed and removed some links in menu.json, fixed settings.py postgres crashing error

--- src/_main_/settings.py
+++ src/_main_/settings.py
@@ -143,7 +143,7 @@ SESSION_COOKIE_SECURE = False
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
-    'production': {
+    'default': {
         'ENGINE': os.environ.get('DATABASE_ENGINE'),
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
@@ -151,7 +151,7 @@ DATABASES = {
         'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': os.environ.get('DATABASE_PORT')
     },
-    'default': {
+    'test': {
         'ENGINE': os.environ.get('DATABASE_ENGINE'),
         'NAME': 'postgres',
         'USER': 'postgres',
