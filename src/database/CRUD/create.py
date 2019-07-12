b35e306faf0bc6d60f53a0a30deef8e1b3e609b4
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-07-12 12:42:22 -0400

    Update create.py
    writing custom create function

--- src/database/CRUD/create.py
+++ src/database/CRUD/create.py
@@ -7,8 +7,16 @@ and to prevent code redundancy.
 from  database.models import *
 from database.utils.common import ensure_required_fields
 
-CREATE_ERROR_MSG = "An error occured during creation.  Please check your \
+CREATE_ERROR_MSG = "An error occurred during creation.  Please check your \
     information and try again"
+
+def create_one_object(model, fields, required_fields):
+  pass
+
+
+def create_multiple_objects(model, data_list):
+  pass
+
 
 def new_action(args):
   required_fields = ['title']
@@ -21,8 +29,19 @@ def new_action(args):
     new_action = Action.objects.create()
     new_action.title = args["title"]
     new_action.save()
-    return {"success": True, "new_action":new_action, "errors":None}
+    return {"success": True, "action":new_action, "errors":None}
   except Exception as e:
     return {"success": False, "errors": [CREATE_ERROR_MSG, str(e)]}
 
-  
+def new_community(args):
+  required_fields = ['name', 'domain']
+  errors = ensure_required_fields(required_fields, args)
+  if errors:
+    return {"success": False, "errors":errors}
+
+  #if code gets here we have everything we need
+  try:
+    new_action = Community.objects.create()
+    return {"success": True, "new_action":new_action, "errors":None}
+  except Exception as e:
+    return {"success": False, "errors": [CREATE_ERROR_MSG, str(e)]}
