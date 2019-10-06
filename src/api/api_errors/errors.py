aa3c0d0216f791b0194fafa80a6ebf95b691a156
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-10-05 23:31:43 -0600

    ADDING MORE ERROR TYPES

--- src/api/api_errors/errors.py
+++
@@ -1,3 +0,0 @@
-INVALID_FIELD_NAME = ""
-INVALID_RESOURCE = "This resource does not exist or you do not have access to it"
-NOT_AUTHORIZED = "You are not authorized to access this resource"
--- src/api/api_errors/massenergize_errors.py
+++ src/api/api_errors/massenergize_errors.py
@@ -6,8 +6,18 @@ class MassEnergizeAPIError(Error):
     f"Error: {self.msg}"
 
 
-class ResourceNotFound(MassEnergizeAPIError):
+class ResourceNotFoundError(MassEnergizeAPIError):
   def __init__(self):
     super().__init__("RESOURCE NOT FOUND ERROR")
 
-  
+
+class NotAuthorizedError(MassEnergizeAPIError):
+  def __init__(self):
+    super().__init__("NOT AUTHORIZED TO ACCESS THIS RESOURCE")
+
+
+
+class InvalidResourceError(MassEnergizeAPIError):
+  def __init__(self):
+    super().__init__("INVALID RESOURCE")
+
