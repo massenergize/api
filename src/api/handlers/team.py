d87032a302d1d24de1a78bbf4677f49f52714627
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-10-05 23:58:07 -0600

    adding api version and updates to team handler

--- src/api/handlers/team.py
+++ src/api/handlers/team.py
@@ -1,1 +1,16 @@
 """Handler file for all routes pertaining to teams"""
+
+from api.utils.route_handler import RouteHandler
+
+class TeamHandler(RouteHandler):
+
+  def registerRoutes(self) -> None:
+    self.add("/teams.info", self.info) 
+
+
+  def info(self) -> None:
+    def info_view(request): 
+      pass
+
+    return info_view
+
