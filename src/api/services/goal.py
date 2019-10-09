d38c36b148246768ffc6aa00cfbaeaa05d141675
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-10-09 09:08:06 -0600

    adding spices to goal creation

--- src/api/services/goal.py
+++ src/api/services/goal.py
@@ -1,6 +1,8 @@
-from api.api_errors.massenergize_errors import MassEnergizeAPIError
+from api.api_errors.massenergize_errors import MassEnergizeAPIError, CustomMassenergizeError
 from api.utils.massenergize_response import MassenergizeResponse
 from api.store.goal import GoalStore
+from api.store.team import TeamStore
+from api.store.community import CommunityStore
 
 class GoalService:
   """
@@ -9,21 +11,45 @@ class GoalService:
 
   def __init__(self):
     self.store =  GoalStore()
+    self.team_store = TeamStore()
+    self.community_store = CommunityStore()
 
+  def validate(self, args):
+    return True 
+
+  
   def get_goal_info(self, goal_id) -> (dict, MassEnergizeAPIError):
     goal, err = self.store.get_goal_info(goal_id)
     if err:
       return None, err
     return goal, None
 
-  def list_goals(self, goal_id) -> (list, MassEnergizeAPIError):
-    goal, err = self.store.list_goals(goal_id)
+  def list_goals(self, user_id) -> (list, MassEnergizeAPIError):
+    goal, err = self.store.list_goals(user_id)
     if err:
       return None, err
     return goal, None
 
 
-  def create_goal(self, args) -> (dict, MassEnergizeAPIError):
+  def create_goal(self, user_id, community_id, team_id, args) -> (dict, MassEnergizeAPIError):
+    if community_id:
+      #this is a community goal
+      #validate the args
+
+      #find the community
+      #create the goal
+      #set the goal in community
+      pass
+    elif team_id:
+      #this is a team goal
+      pass 
+    elif user_id:
+      #this is a user goal
+      pass 
+
+    else:
+      return None, CustomMassenergizeError("You have to provide specify a community, team or user")
+    
     goal, err = self.store.create_goal(args)
     if err:
       return None, err
