2dffb2b5b85d781c0169fcf8d5e5619dc4b0b6c8
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-10-10 09:55:02 -0600

    increasing and decreasing goals working, changed models

--- src/api/store/goal.py
+++ src/api/store/goal.py
@@ -1,6 +1,7 @@
 from database.models import Goal, UserProfile, Team, Community
 from api.api_errors.massenergize_errors import MassEnergizeAPIError, InvalidResourceError, ServerError, CustomMassenergizeError
 from api.utils.massenergize_response import MassenergizeResponse
+from django.db.models import F
 
 class GoalStore:
   def __init__(self):
@@ -15,58 +16,126 @@ class GoalStore:
 
 
   def list_goals(self, community_id, team_id, user_id) -> (list, MassEnergizeAPIError):
-    goals = []
-    if community_id:
-      community = Community.objects.filter(id=community_id).first()
-      if community and community.goal:
-        goals.append(community.goal)
-    elif team_id:
-      #this is a team goal
-      team = Team.objects.filter(id=team_id).first()
-      if team and team.goal:
-        goals.append(team.goal)
-    elif user_id:
-      user = UserProfile.objects.filter(id=user_id)
-      if user and user.goal:
-        goals.append(user.goal)
-    else:
-      return None, CustomMassenergizeError("Provide a community, team or user")
-
-    return goals, None
-
-
-  def create_goal(self, args) -> (Goal, MassEnergizeAPIError):
     try:
+      goals = []
+      if community_id:
+        community = Community.objects.filter(id=community_id).first()
+        if not community:
+          return None, CustomMassenergizeError(f"There is no community with id {community_id}")
+        
+        if community.goal and not community.goal.is_deleted:
+          goals.append(community.goal)
+
+      elif team_id:
+        team = Team.objects.filter(id=team_id).first()
+        if not team and not community.goal.is_deleted:
+          return None, CustomMassenergizeError(f"There is no community with id {community_id}")
+
+        if team.goal:
+          goals.append(team.goal)
+
+      elif user_id:
+        user = UserProfile.objects.filter(id=user_id).first()
+        if not user:
+          return None, CustomMassenergizeError(f"There is no community with id {community_id}")
+        if user.goal and not community.goal.is_deleted:
+          goals.append(user.goal)
+
+      else:
+        return None, CustomMassenergizeError("Provide a community, team or user")
+
+      return goals, None
+    except Exception as e:
+      return None, CustomMassenergizeError(str(e))
+
+
+  def create_goal(self, community_id, team_id, user_id, args) -> (Goal, MassEnergizeAPIError):
+    try:
+      #create the goal
       new_goal = Goal.objects.create(**args)
-      new_goal.save()
+
+      if community_id:
+        community = Community.objects.filter(id=community_id).first()
+        if not community:
+          return None, CustomMassenergizeError(f"There is no community with id {community_id}")
+        
+        new_goal.save()
+        community.goal = new_goal
+        community.save()
+
+      elif team_id:
+        team = Team.objects.filter(id=team_id).first()
+        if not team:
+          return None, CustomMassenergizeError(f"There is no team with id {team_id}")
+
+        new_goal.save()
+        team.goal = new_goal
+        team.save()
+
+      elif user_id:
+        user = UserProfile.objects.filter(id=user_id).first()
+        if not user:
+          return None, CustomMassenergizeError(f"There is no user with id {user_id}")
+        new_goal.save()
+        user.goal = new_goal
+        user.save()
+
+      else:
+        return None, CustomMassenergizeError("Provide a community, team or user")
+
       return new_goal, None
     except Exception as e:
-      print(e)
-      return None, ServerError()
-
+      return None, CustomMassenergizeError(str(e))
 
   def update_goal(self, goal_id, args) -> (Goal, MassEnergizeAPIError):
-    goal = Goal.objects.filter(id=goal_id)
-    if not goal:
-      return None, InvalidResourceError()
-    goal.update(**args)
-    return goal.full_json(), None
-
+    try:
+      #find the goal
+      goal = Goal.objects.filter(id=goal_id)
+      if not goal:
+        return None, InvalidResourceError()
+      goal.update(**args)
+      return goal.first(), None
+    except Exception as e:
+      return None, CustomMassenergizeError(str(e))
 
   def delete_goal(self, goal_id) -> (Goal, MassEnergizeAPIError):
-    goals = Goal.objects.filter(id=goal_id)
-    if not goals:
-      return None, InvalidResourceError()
-
+    try:
+      #find the goal
+      goal = Goal.objects.filter(id=goal_id).update(is_deleted=True)
+      if not goal:
+        return None, InvalidResourceError()
+      return goal.first(), None
+    except Exception as e:
+      return None, CustomMassenergizeError(str(e))
 
   def list_goals_for_community_admin(self, community_id) -> (list, MassEnergizeAPIError):
-    goals = Goal.objects.filter(community__id = community_id)
-    return [t.simple_json() for t in goals], None
+    return self.list_goals(community_id, None, None)
 
 
   def list_goals_for_super_admin(self):
     try:
-      goals = Goal.objects.all()
-      return [t.simple_json() for t in goals], None
+      return Goal.objects.all(is_deleted=False)
+    except Exception as e:
+      return None, CustomMassenergizeError(str(e))
+
+
+  def increase_value(self, goal_id, field_name):
+    try:
+      goals_to_incr =  Goal.objects.filter(id=goal_id)
+      goals_to_incr.update(**{field_name: F(field_name)+1})
+      if not goals_to_incr:
+        return None, InvalidResourceError()
+      return goals_to_incr.first(), None
+    except Exception as e:
+      return None, CustomMassenergizeError(str(e))
+
+
+  def decrease_value(self, goal_id, field_name):
+    try:
+      goals_to_decrease =  Goal.objects.filter(id=goal_id)
+      goals_to_decrease.update(**{field_name: F(field_name)-1})
+      if not goals_to_decrease:
+        return None, InvalidResourceError()
+      return goals_to_decrease.first(), None
     except Exception as e:
       return None, CustomMassenergizeError(str(e))
