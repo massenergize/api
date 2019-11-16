71baa7cb9a6654049ad2b0926f153869b04d18f4
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-11-16 15:58:19 -0700

    updating validator function

--- src/_main_/utils/validator/__init__.py
+++ src/_main_/utils/validator/__init__.py
@@ -4,50 +4,66 @@ from _main_.utils.common import parse_bool, parse_date, parse_list, parse_int, p
 class Validator:
 
   def __init__(self):
-    self.fields = []
+    self.fields = {}
+    self.rename_fields = set()
 
-  def add(self, field_name, field_type=str, is_required=True):
-    self.fields.append({
-      "name": field_name,
-      "type": field_type,
-      "is_required": is_required
-    })
-    return self
 
   def expect(self, field_name, field_type=str, is_required=True):
-    self.fields.append({
-      "name": field_name,
+    self.fields[field_name] = {
       "type": field_type,
       "is_required": is_required
-    })
+    }
     return self
 
+  def expect_all(self, lst):
+    for (field_name, field_type, is_required) in lst:
+      self.fields[field_name] = {
+        "type": field_type,
+        "is_required": is_required
+      }
+    return self
+
+  def rename(self, old_name, new_name):
+    self.rename_fields.add((old_name, new_name))
+    return self
 
 
   def _common_name(self, s):
     return (' '.join(s.split('_'))).title()
 
   def verify(self, args):
-    for field in self.fields:
-      field_name = field["name"]
-      field_type = field["type"]
-      field_is_required = field["is_required"]
+    try:
+      #first rename all fields that need renaming
+      for (old_name, new_name) in self.rename_fields:
+        val = self.fields.pop(old_name, None)
+        if val:
+          self.fields[new_name] = val
 
-      if field_is_required and field_name not in args:
-        return None, CustomMassenergizeError(f"You are Missing a Required Input: {self._common_name(field_name)}")
+      # cleanup and verify all contents of the args and return it
+      for field_name, field_info in self.fields.items():
+        field_type = field_info["type"]
+        field_is_required = field_info["is_required"]
+
+        if field_is_required and field_name not in args:
+          return None, CustomMassenergizeError(f"You are Missing a Required Input: {self._common_name(field_name)}")
+        
+        if field_name in args:
+          if field_type == str:
+            args[field_name] = parse_string(args[field_name])
+          elif field_type == int:
+            args[field_name] = parse_int(args[field_name])
+          elif field_type == bool:
+            args[field_name] = parse_bool(args[field_name])
+          elif field_type == list:
+            args[field_name] = parse_list(args[field_name])
+          elif field_type == 'date':
+            args[field_name] = parse_date(args[field_name])
+        else:
+          if field_type == 'location':
+            args[field_name] = parse_location(args)
+
+      return args, None
       
-      if field_name in args:
-        if field_type == str:
-          args[field_name] = parse_string(args[field_name])
-        elif field_type == int:
-          args[field_name] = parse_int(args[field_name])
-        elif field_type == bool:
-          args[field_name] = parse_bool(args[field_name])
-        elif field_type == list:
-          args[field_name] = parse_list(args[field_name])
-        elif field_type == 'date':
-          args[field_name] = parse_date(args[field_name])
-      else:
-        if field_type == 'location':
-          args[field_name] = parse_location(args)
-    return args, None
+    except Exception as e:
+      print(e)
+      return None, CustomMassenergizeError(e)
