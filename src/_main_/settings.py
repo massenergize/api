0026723dbec5423709f011037f7abf18649fc58f
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2020-04-19 10:29:53 -0600

    upgrading libraries

--- src/_main_/settings.py
+++ src/_main_/settings.py
@@ -29,7 +29,7 @@ IS_PROD = False
 SECRET_KEY =  os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
