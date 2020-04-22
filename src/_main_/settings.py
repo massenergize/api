675e1c146342d7944d741779cb0caf64d2ac8c11
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2020-04-21 21:06:13 -0600

    skrrrrr

--- src/_main_/settings.py
+++ src/_main_/settings.py
@@ -182,10 +182,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True 
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587 
-EMAIL_HOST_USER = os.environ.get('EMAIL') 
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL')
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
