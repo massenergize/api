88fb6e9f3ebdb4f926cece166ca7f51818840a50
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2020-05-08 00:05:32 -0600

    trap4

--- src/_main_/settings.py
+++ src/_main_/settings.py
@@ -145,7 +145,7 @@ DATABASES = {
         'PORT': '5432'
     },
 }
-
+print(len(os.environ.get('FIREBASE_SERVICE_ACCOUNT_CLIENT_URL', '')))
 FIREBASE_CREDENTIALS = credentials.Certificate({
   "type": "service_account",
   "project_id": os.environ.get('FIREBASE_SERVICE_ACCOUNT_PROJECT_ID'),
