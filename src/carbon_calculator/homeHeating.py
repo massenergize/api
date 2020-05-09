249f122240862ae147f6da5872cc0b0ec28d2b51
Brad Hubbard-Nelson <bradhn@mindspring.com>
2020-05-09 06:35:56 -0400

    Bug fixes

--- src/carbon_calculator/homeHeating.py
+++ src/carbon_calculator/homeHeating.py
@@ -224,7 +224,9 @@ def EvalAirSourceHeatPump(inputs):
         utility_rebates = getDefault(locality,'heating_ashp_utility_rebate', 2500.)
         cost = cost * (1 - tax_credit) - utility_rebates
 
-        payback = cost / savings
+        payback = 100
+        if savings > 0:
+            payback = cost / savings
         if payback > 0 and payback < 10:
             explanation = "Installing an air-source heat pump system for your home could save %d tons of CO2 per year, and pay for itself in around %d years." % (int(points/2000), int(payback))
         else:
