ddc63d506c43dc0ee533638ad6784f46fe90cfff
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2020-07-02 22:45:25 -0600

    configure sentry in settings and setup in error views

--- src/website/views.py
+++ src/website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from _main_.utils.massenergize_response import MassenergizeResponse
 from database.models import Deployment
 from _main_.settings import IS_PROD
+from sentry_sdk import capture_message
 
 # Create your views here.
 def home(request):
@@ -25,13 +26,17 @@ def home(request):
   )
 
 def handler400(request, exception):
+  capture_message("Bad Request", level="error")
   return MassenergizeResponse(error="Error: BadRequest")
 
 def handler403(request, exception):
+  capture_message("Permission Denied", level="error")
   return MassenergizeResponse(error="Error: PermissionDenied")
 
 def handler404(request, exception):
+  capture_message("Page not found!", level="error")
   return MassenergizeResponse(data=f"path: {request.build_absolute_uri()}", error="Error: ResourceNotFound")
 
 def handler500(request):
+  capture_message("Server Error", level="error")
   return MassenergizeResponse(error="Error: ServerError")
