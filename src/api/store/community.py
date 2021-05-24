abfa0a32535abbaca7afe32b087eefbf831f9308
frimpongopoku <pongofrimi@gmail.com>
2021-05-24 15:59:05 +0400

    Tiny config rest here

--- src/api/store/community.py
+++ src/api/store/community.py
@@ -341,7 +341,6 @@ class CommunityStore:
 
 
   def list_communities(self, context: Context, args) -> (list, MassEnergizeAPIError):
-    print("<---------I am the shege guy bro --------->")
     try:
       if context.is_sandbox:
         communities = Community.objects.prefetch_related("locations").filter(is_deleted=False, is_approved=True).exclude(subdomain='template')
