6ab3c96917a560d72b356ea14e27c5c4114bbdf5
IshaSangani <51543957+IshaSangani@users.noreply.github.com>
2021-06-19 10:14:32 -0700

    automatic email invitation

--- src/api/handlers/userprofile.py
+++ src/api/handlers/userprofile.py
@@ -1,5 +1,7 @@
 """Handler file for all routes pertaining to users"""
-from database.models import UserProfile
+from functools import wraps
+from _main_.utils.emailer.send_email import send_massenergize_email
+from database.models import CommunityAdminGroup, UserProfile, Community
 from _main_.utils.route_handler import RouteHandler
 from _main_.utils.common import get_request_contents, rename_field
 from api.services.userprofile import UserService
@@ -8,7 +10,7 @@ from _main_.utils.massenergize_errors import MassEnergizeAPIError, InvalidResour
 from types import FunctionType as function
 from _main_.utils.context import Context
 from _main_.utils.validator import Validator
-from api.decorators import admins_only, super_admins_only, login_required
+from api.decorators import admins_only, community_admins_only, super_admins_only, login_required
 from sentry_sdk import capture_message
 # import pandas as pd
 # for import contacts endpoint - accepts a csv file and verifies correctness of email address format
@@ -40,12 +42,12 @@ class UserHandler(RouteHandler):
     self.add("/users.households.remove", self.remove_household)
     self.add("/users.households.list", self.list_households)
     self.add("/users.events.list", self.list_events)
-    self.add("/users.import", self.handle_contacts_csv)
-
 
     #admin routes
     self.add("/users.listForCommunityAdmin", self.community_admin_list)
     self.add("/users.listForSuperAdmin", self.super_admin_list)
+    self.add("/users.import", self.handle_contacts_csv)
+    self.add("/users.test", self.test_route)
 
 
   @login_required
@@ -236,14 +238,24 @@ class UserHandler(RouteHandler):
       return MassenergizeResponse(error=str(err), status=err.status)
     return MassenergizeResponse(data=user_info)
   
+
   @admins_only
-  def handle_contacts_csv(self, request):
+  # @community_admins_only
+  def handle_contacts_csv(self, request) -> (dict, MassEnergizeAPIError):
     context: Context = request.context
     args: dict = context.args
-    print("these are the contents of the request")
-    print(args)
+    # query users by user id, find the user that is sending the request
+    user = UserProfile.objects.filter(id="837ef9ff-4065-4a41-b5d3-cfba7abc9108").first()
+    # find the community that the user is the admin of. In the next section, populate user profiles with that information
+    registered_community = None
+    print(user.communities.all())
+    for community in user.communities.all():
+        admin_group = CommunityAdminGroup.objects.filter(community=community).first()
+        if user in admin_group.members.all():
+          break
+    
+    registered_community = community
     csv_ref = args['csv'].file    
-    print(type(csv_ref))
     # csv_ref is a bytes object, we need a csv
     # so we copy it as a csv temporarily to the disk
     temporarylocation="testout.csv"
@@ -252,16 +264,14 @@ class UserHandler(RouteHandler):
       var = csv_ref.read()
       # print(var)
       out.write(var)
-
     with open(temporarylocation, "r") as f:
       reader = csv.DictReader(f, delimiter=",")
       for row in reader:
+        column_list = list(row.keys())
         try:
           # prevents the first row (headers) from being read in as a user
-          if row['First Name'] == 'First Name':
+          if row['First Name'] == column_list[0]:
             continue
-          print('this is a row')
-          print(row)
           # verify correctness of email address
           regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
           try: 
@@ -277,11 +287,15 @@ class UserHandler(RouteHandler):
                   is_vendor = False, 
                   accepts_terms_and_conditions = False
                 )   
+                if registered_community:
+                  new_user.communities.add(registered_community)
               else: 
-                new_user: UserProfile = user   
-              new_user.save()    
-            else:   
-                print("Invalid Email")  
+                new_user: UserProfile = user  
+              new_user.save()  
+              # send email inviting user to complete their profile
+              message = user.full_name + " invited you to join the following MassEnergize Community: " + registered_community
+              send_massenergize_email(subject="Invitation to Join MassEnergize Community", msg=message, to=new_user.email)
+            else:    
                 return None, CustomMassenergizeError("Valid email required for sign up")
           except Exception as e:
             capture_message(str(e), level="error")
@@ -289,5 +303,44 @@ class UserHandler(RouteHandler):
             return None, CustomMassenergizeError(e)
         except Exception as e:
           print(str(e))
+          return None, CustomMassenergizeError(e)
     # and then delete it once we are done parsing it
     os.remove(temporarylocation)
+    res = {'column names' : column_list}
+    return MassenergizeResponse(data=res)
+
+ 
+  def test_route(self, request):
+    try:
+      print('route is working up to here')
+    except Exception as e:
+      print(str(e))
+    try: 
+      registered_community = Community.objects.filter(name="Sustainable Students").first()
+      user = UserProfile.objects.filter(email="arthorse17@gmail.com").first()
+      if not user:
+        new_user: UserProfile = UserProfile.objects.create(
+          full_name = "Patrick Star", 
+          preferred_name = "Patrick", 
+          email = "arthorse17@gmail.com",
+          is_vendor = False, 
+          accepts_terms_and_conditions = False
+        )   
+        if registered_community:
+          new_user.communities.add(registered_community)
+      else: 
+        new_user: UserProfile = user  
+      new_user.save()  
+      # send email inviting user to complete their profile
+      print("type of user")
+      print(type(user))
+      message = user.full_name + " invited you to join the following MassEnergize Community: " + registered_community.name
+      send_massenergize_email(subject="Invitation to Join MassEnergize Community", msg=message, to=new_user.email)
+      print("email sent")
+      dat = {"lovely":"lovely"}
+      return MassenergizeResponse(data=dat)
+    except Exception as e:
+      capture_message(str(e), level="error")
+      print(str(e))
+      err = str(e)
+      return MassenergizeResponse(error=err)
