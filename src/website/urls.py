caa39165ec939e7a59bb9a9ebeab8290cb251b53
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2021-08-12 08:13:16 -0700

    adding urls

--- src/website/urls.py
+++ src/website/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 
-from website.views import home, generate_sitemap
+from website import views
 
 urlpatterns = [
-  path('', home, name='home'),
-  path('sitemap', generate_sitemap, name='generate_sitemap'),
+  path('', views.communities, name='home'),
+  path('communities', views.communities, name='communities'),
+  path('community/<slug:subdomain>', views.communities, name='communities'),
+  path('actions/<slug:subdomain>', views.communities, name='communities'),
+  path('action/<slug:subdomain>', views.communities, name='communities'),
+  path('events/<slug:subdomain>', views.communities, name='communities'),
+  path('event/<slug:subdomain>', views.communities, name='communities'),
+  path('services/<slug:subdomain>', views.communities, name='communities'),
+  path('service/<slug:subdomain>', views.communities, name='communities'),
+  path('testimonials/<slug:subdomain>', views.communities, name='communities'),
+  path('testimonial/<slug:subdomain>', views.communities, name='communities'),
+  path('teams/<slug:subdomain>', views.communities, name='communities'),
+  path('team/<slug:subdomain>', views.communities, name='communities'),
+  path('sitemap', views.generate_sitemap, name='generate_sitemap'),
 ]
