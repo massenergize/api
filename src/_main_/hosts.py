be6074d5a126e2b3181b68d73078fcb2bde30d2d
Brad Hubbard-Nelson <bradhn@mindspring.com>
2021-09-07 10:49:11 -0400

    Fix canary api

--- src/_main_/hosts.py
+++ src/_main_/hosts.py
@@ -8,4 +8,5 @@ host_patterns = patterns(
   host(r'www', settings.ROOT_URLCONF, name='www'),
   host(r'api', 'api.urls', name='api'),
   host(r'api-dev', 'api.urls', name='api-dev'),
+  host(r'api-canary', 'api.urls', name='api-canary'),
 )
