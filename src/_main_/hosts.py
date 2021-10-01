89311a21d1ccc5500d43ebc84426c8c14e9ac3dd
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2021-09-30 21:14:14 -0700

    add subdomains to host

--- src/_main_/hosts.py
+++ src/_main_/hosts.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django_hosts import patterns, host
 
-
 host_patterns = patterns(
   '',
   host(r'api', 'api.urls', name='api'),
   host(r'api-dev', 'api.urls', name='api-dev'),
   host(r'api-canary', 'api.urls', name='api-canary'),
   host(r'communities', settings.ROOT_URLCONF, name=settings.DEFAULT_HOST),
+  host(r'mc', '_main_.admin_urls', name='mc'),
+  host(r'missioncontrol', '_main_.admin_urls', name='missioncontrol'),
+  host(r'admin', '_main_.admin_urls', name='admin'),
 )
