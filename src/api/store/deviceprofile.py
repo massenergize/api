f5784ee975b6e1ddd4f6bd7b77fc8b6774cd3d27
Dave Kinghorn <davidreact@gmail.com>
2021-10-07 12:09:58 -0400

    create_device store function

--- src/api/store/deviceprofile.py
+++ src/api/store/deviceprofile.py
@@ -29,11 +29,33 @@ class DeviceStore:
   def create_device(self, context: Context, args) -> Tuple[dict, MassEnergizeAPIError]:
     try:
       ip_address = args.pop('ip_address', context.ip_address)
-      ip_address = args.pop('ip_address', context.ip_address)
-      ip_address = args.pop('ip_address', context.ip_address)
-      ip_address = args.pop('ip_address', context.ip_address)
-      ip_address = args.pop('ip_address', context.ip_address)
+      device_type = args.pop('device_type', context.device_type)
+      operating_system = args.pop('operating_system', context.operating_system)
+      browser = args.pop('browser', context.browser)
+      browser_version = args.pop('browser_version', context.browser_version)
+     
+      new_device_profile: DeviceProfile = DeviceProfile.objects.create(**args)
 
+      user = None
+
+      if ip_address:
+        # TODO: Anything we want to do with a device's IP address can happen here
+        new_device_profile.ip_address = ip_address
+
+      if device_type:
+        new_device_profile.device_type = device_type
+
+      if operating_system:
+        new_device_profile.operating_system = operating_system
+
+      if browser:
+        new_device_profile.browser = browser
+
+      if browser_version:
+        new_device_profile.browser_version = browser_version
+
+      new_device_profile.save()    
+      return new_device_profile, None
       
     except Exception as e:
       capture_message(str(e), level="error")
