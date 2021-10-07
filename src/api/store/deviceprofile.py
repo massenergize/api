9ccad5854a0cdad9f5fdd507dae7c3713dbcc3c7
Dave Kinghorn <davidreact@gmail.com>
2021-10-07 12:36:17 -0400

    update_device_profile

--- src/api/store/deviceprofile.py
+++ src/api/store/deviceprofile.py
@@ -22,41 +22,62 @@ class DeviceStore:
       if not device:
         return None, InvalidResourceError()
       return device, None
+
     except Exception as e:
       capture_message(str(e), level="error")
       return None, CustomMassenergizeError(e)
   
-  def create_device(self, context: Context, args) -> Tuple[dict, MassEnergizeAPIError]:
-    try:
-      ip_address = args.pop('ip_address', context.ip_address)
-      device_type = args.pop('device_type', context.device_type)
-      operating_system = args.pop('operating_system', context.operating_system)
-      browser = args.pop('browser', context.browser)
-      browser_version = args.pop('browser_version', context.browser_version)
-     
-      new_device_profile: DeviceProfile = DeviceProfile.objects.create(**args)
+  def __device_profile_attr_handler(new_device_profile, args, context):
+    ip_address = args.pop('ip_address', context.ip_address)
+    device_type = args.pop('device_type', context.device_type)
+    operating_system = args.pop('operating_system', context.operating_system)
+    browser = args.pop('browser', context.browser)
+    browser_version = args.pop('browser_version', context.browser_version)
 
-      user = None
-
-      if ip_address:
+    if ip_address:
         # TODO: Anything we want to do with a device's IP address can happen here
         new_device_profile.ip_address = ip_address
 
-      if device_type:
-        new_device_profile.device_type = device_type
+    if device_type:
+      new_device_profile.device_type = device_type
 
-      if operating_system:
-        new_device_profile.operating_system = operating_system
+    if operating_system:
+      new_device_profile.operating_system = operating_system
 
-      if browser:
-        new_device_profile.browser = browser
+    if browser:
+      new_device_profile.browser = browser
 
-      if browser_version:
-        new_device_profile.browser_version = browser_version
+    if browser_version:
+      new_device_profile.browser_version = browser_version
+    
+  def create_device(self, context: Context, args) -> Tuple[dict, MassEnergizeAPIError]:
+    try:     
+      new_device_profile: DeviceProfile = DeviceProfile.objects.create(**args)
+
+      self.__device_profile_attr_handler(new_device_profile, args, context)
 
       new_device_profile.save()    
       return new_device_profile, None
+
+    except Exception as e:
+      capture_message(str(e), level="error")
+      return None, CustomMassenergizeError(e)
       
+  def update_device_profile(self, context: Context, args) -> Tuple[dict, MassEnergizeAPIError]:
+    try:
+      id = args.pop("id", None)
+      device_profile = DeviceProfile.objects.filter(id=id)
+      if not device_profile:
+        return None, InvalidResourceError()
+      
+      device_profile.update(**args)
+      new_device_profile = device_profile.first()
+
+      self.__device_profile_attr_handler(new_device_profile, args, context)
+
+      new_device_profile.save()
+      return new_device_profile, None
+
     except Exception as e:
       capture_message(str(e), level="error")
       return None, CustomMassenergizeError(e)
