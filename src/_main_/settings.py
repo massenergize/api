b77e4df0d48dafcf7a5467d1452d6b45cd41b679
Brad Hubbard-Nelson <bradhn@mindspring.com>
2021-10-21 06:38:01 -0400

    Test DEBUG=true for other cookie settins

--- src/_main_/settings.py
+++ src/_main_/settings.py
@@ -47,7 +47,7 @@ except Exception:
 SECRET_KEY =  os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
