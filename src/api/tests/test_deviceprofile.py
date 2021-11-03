2a1d0e867d4e052df6eff921e97291f543e8e016
Dave Kinghorn <davidreact@gmail.com>
2021-11-03 16:24:08 -0400

    Tests written

--- src/api/tests/test_deviceprofile.py
+++ src/api/tests/test_deviceprofile.py
@@ -30,117 +30,153 @@ class DeviceHandlerTest(TestCase):
 
     def setUp(self):
       # this gets run on every test case
-      pass
+      signinAs(self.client, None)
+
+      create_response = self.client.post('/api/device.create', urlencode({}), content_type="application/x-www-form-urlencoded").toDict()
+      devices = DeviceProfile.objects.filter(id=create_response["data"]["id"])
+      self.device = devices.first()
+
+      create_response = self.client.post('/api/device.create', urlencode({}), content_type="application/x-www-form-urlencoded").toDict()
+      devices = DeviceProfile.objects.filter(id=create_response["data"]["id"])
+      self.device1 = devices.first()
+
+      create_response = self.client.post('/api/device.create', urlencode({}), content_type="application/x-www-form-urlencoded").toDict()
+      devices = DeviceProfile.objects.filter(id=create_response["data"]["id"])
+      self.device2 = devices.first()
+
+      create_response = self.client.post('/api/device.create', urlencode({}), content_type="application/x-www-form-urlencoded").toDict()
+      devices = DeviceProfile.objects.filter(id=create_response["data"]["id"])
+      self.device3 = devices.first()
 
     def test_create(self):
       # test create not logged in
       signinAs(self.client, None)
       create_response = self.client.post('/api/device.create', urlencode({}), content_type="application/x-www-form-urlencoded").toDict()
       self.assertTrue(create_response["success"])
+      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": create_response["data"]["id"]}), content_type="application/x-www-form-urlencoded").toDict()
 
       # test create logged as user
       signinAs(self.client, self.USER)
       create_response = self.client.post('/api/device.create', urlencode({}), content_type="application/x-www-form-urlencoded").toDict()
       self.assertTrue(create_response["success"])
+      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": create_response["data"]["id"]}), content_type="application/x-www-form-urlencoded").toDict()
 
       # test create logged as cadmin
       signinAs(self.client, self.CADMIN)
       create_response = self.client.post('/api/device.create', urlencode({}), content_type="application/x-www-form-urlencoded").toDict()
       self.assertTrue(create_response["success"])
+      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": create_response["data"]["id"]}), content_type="application/x-www-form-urlencoded").toDict()
 
       # test create logged as sadmin
       signinAs(self.client, self.SADMIN)
       create_response = self.client.post('/api/device.create', urlencode({}), content_type="application/x-www-form-urlencoded").toDict()
       self.assertTrue(create_response["success"])
+      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": create_response["data"]["id"]}), content_type="application/x-www-form-urlencoded").toDict()
     
     def test_info(self):
+
       # test info not logged in
       signinAs(self.client, None)
-      info_response = self.client.post('/api/device.info', urlencode({"id": self.device1.id}), content_type="application/x-www-form-urlencoded").toDict()
+      info_response = self.client.post('/api/device.info', urlencode({"id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
       self.assertTrue(info_response["success"])
 
       # test info logged as user
       signinAs(self.client, self.USER)
-      info_response = self.client.post('/api/device.info', urlencode({"id": self.device1.id}), content_type="application/x-www-form-urlencoded").toDict()
+      info_response = self.client.post('/api/device.info', urlencode({"id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
       self.assertTrue(info_response["success"])
 
       # test info logged as cadmin
       signinAs(self.client, self.CADMIN)
-      info_response = self.client.post('/api/device.info', urlencode({"id": self.device1.id}), content_type="application/x-www-form-urlencoded").toDict()
+      info_response = self.client.post('/api/device.info', urlencode({"id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
       self.assertTrue(info_response["success"])
 
       # test info logged as sadmin
       signinAs(self.client, self.SADMIN)
-      info_response = self.client.post('/api/device.info', urlencode({"id": self.device1.id}), content_type="application/x-www-form-urlencoded").toDict()
+      info_response = self.client.post('/api/device.info', urlencode({"id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
       self.assertTrue(info_response["success"])
 
-    def test_update(self):
-      # test update not signed in
-      signinAs(self.client, None)
-      update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device1.id, "title": "none_title"}), content_type="application/x-www-form-urlencoded").toDict()
-      self.assertFalse(update_response["success"])
-
-      # test update signed as user
-      signinAs(self.client, self.USER)
-      update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device1.id, "title": "user_title"}), content_type="application/x-www-form-urlencoded").toDict()
-      self.assertFalse(update_response["success"])
-
-      # test update as cadmin
-      signinAs(self.client, self.CADMIN)
-      update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device1.id, "title": "cadmin_title"}), content_type="application/x-www-form-urlencoded").toDict()
-      self.assertTrue(update_response["success"])
-      self.assertEquals(update_response["data"]["title"], "cadmin_title")
-
-      # test update as sadmin
-      signinAs(self.client, self.SADMIN)
-      update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device1.id, "title": "sadmin_title"}), content_type="application/x-www-form-urlencoded").toDict()
-      self.assertTrue(update_response["success"])
-      self.assertEquals(update_response["data"]["title"], "sadmin_title")
+    # def test_update(self): # Currently update does nothing since data is grabbed from the back end
+    #   # test update not signed in
+    #   signinAs(self.client, None)
+    #   update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
+    #   self.assertTrue(update_response["success"])
+    #   
+    #   # test update signed as user
+    #   signinAs(self.client, self.USER)
+    #   update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
+    #   self.assertTrue(update_response["success"])
+    #   
+    #   # test update as cadmin
+    #   signinAs(self.client, self.CADMIN)
+    #   update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
+    #   self.assertTrue(update_response["success"])
+    #   self.assertEquals(update_response["data"]["title"], "cadmin_title")
+    #   
+    #   # test update as sadmin
+    #   signinAs(self.client, self.SADMIN)
+    #   update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
+    #   self.assertTrue(update_response["success"])
+    #   self.assertEquals(update_response["data"]["title"], "sadmin_title")
     
-    def test_log(self):
+    def test_device_log(self):
+      visit_log = self.device.visit_log
+      visit_logs = len(visit_log)
+
       # test update not signed in
       signinAs(self.client, None)
-      update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device1.id, "title": "none_title"}), content_type="application/x-www-form-urlencoded").toDict()
-      self.assertFalse(update_response["success"])
+      log_response = self.client.post('/api/device.log', urlencode({"device_id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
+      visit_logs += 1
+      self.assertFalse(log_response["success"])
+      self.assertEquals(len(log_response["data"]["visit_log"]), visit_logs)
+    
+    def test_user_log(self):
+      visit_log = self.device.visit_log
+      visit_logs = len(visit_log)
 
-      # test update signed as user
+      # test log signed as user
       signinAs(self.client, self.USER)
-      update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device1.id, "title": "user_title"}), content_type="application/x-www-form-urlencoded").toDict()
-      self.assertFalse(update_response["success"])
+      log_response = self.client.post('/api/device.log', urlencode({"device_id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
+      visit_logs += 1
+      self.assertFalse(log_response["success"])
+      self.assertEquals(len(log_response["data"]["visit_log"]), visit_logs)
 
-      # test update as cadmin
+      # test log as cadmin
       signinAs(self.client, self.CADMIN)
-      update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device1.id, "title": "cadmin_title"}), content_type="application/x-www-form-urlencoded").toDict()
-      self.assertTrue(update_response["success"])
-      self.assertEquals(update_response["data"]["title"], "cadmin_title")
+      log_response = self.client.post('/api/device.log', urlencode({"device_id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
+      visit_logs += 1
+      self.assertTrue(log_response["success"])
+      self.assertEquals(len(log_response["data"]["visit_log"]), visit_logs)
 
-      # test update as sadmin
+      # test log as sadmin
       signinAs(self.client, self.SADMIN)
-      update_response = self.client.post('/api/device.update', urlencode({"device_id": self.device1.id, "title": "sadmin_title"}), content_type="application/x-www-form-urlencoded").toDict()
-      self.assertTrue(update_response["success"])
-      self.assertEquals(update_response["data"]["title"], "sadmin_title")
+      log_response = self.client.post('/api/device.log', urlencode({"device_id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
+      visit_logs += 1
+      self.assertTrue(log_response["success"])
+      self.assertEquals(len(log_response["data"]["visit_log"]), visit_logs)
 
     # device object has no attribute first?
     def test_delete(self):
       # test not signed in
       signinAs(self.client, None)
-      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": self.device3.id}), content_type="application/x-www-form-urlencoded").toDict()
-      self.assertFalse(delete_response["success"])
+      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": self.device.id}), content_type="application/x-www-form-urlencoded").toDict()
+      self.assertTrue(delete_response["success"])
+      self.assertTrue(delete_response["data"]["is_deleted"])
 
       # test as user
       signinAs(self.client, self.USER)
-      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": self.device3.id}), content_type="application/x-www-form-urlencoded").toDict()
-      self.assertFalse(delete_response["success"])
+      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": self.device1.id}), content_type="application/x-www-form-urlencoded").toDict()
+      self.assertTrue(delete_response["success"])
+      self.assertTrue(delete_response["data"]["is_deleted"])
 
       # test as cadmin
       signinAs(self.client, self.CADMIN)
-      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": self.device4.id}), content_type="application/x-www-form-urlencoded").toDict()
+      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": self.device2.id}), content_type="application/x-www-form-urlencoded").toDict()
       self.assertTrue(delete_response["success"])
       self.assertTrue(delete_response["data"]["is_deleted"])
 
       # test as sadmin
       signinAs(self.client, self.SADMIN)
-      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": self.device5.id}), content_type="application/x-www-form-urlencoded").toDict()
+      delete_response = self.client.post('/api/device.delete', urlencode({"device_id": self.device3.id}), content_type="application/x-www-form-urlencoded").toDict()
       self.assertTrue(delete_response["success"])
       self.assertTrue(delete_response["data"]["is_deleted"])
 
