3d5a9ecd269a811286320b6856bf50638699a372
Brad Hubbard-Nelson <bradhn@mindspring.com>
2021-11-12 17:13:41 -0500

    Changes to run locally

--- src/_main_/settings.py
+++ src/_main_/settings.py
@@ -18,6 +18,12 @@ from pathlib import Path  # python3 only
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
+
+DJANGO_ENV = os.environ.get("DJANGO_ENV","remote")
+DEV_SERVER = False
+if DJANGO_ENV == "local":
+    DEV_SERVER = True
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -47,7 +53,8 @@ except Exception:
 SECRET_KEY =  os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = True
+DEBUG = DEV_SERVER
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
