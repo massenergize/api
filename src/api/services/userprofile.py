1cb46b4158b822da42d2d662326b3262fb9bdaf0
Brad Hubbard-Nelson <bradhn@mindspring.com>
2022-01-14 20:12:13 -0500

    420 Notify primary cadmin when testimonial submitted

--- src/api/services/userprofile.py
+++ src/api/services/userprofile.py
@@ -2,7 +2,6 @@ from _main_.utils.massenergize_errors import CustomMassenergizeError, MassEnergi
 from _main_.utils.common import serialize, serialize_all
 from api.store.userprofile import UserStore
 from _main_.utils.context import Context
-from _main_.utils.emailer.send_email import send_massenergize_email
 from _main_.utils.emailer.send_email import send_massenergize_rich_email
 from _main_.utils.constants import COMMUNITY_URL_ROOT
 import os, csv
