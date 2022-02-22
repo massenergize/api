90c9b9628b8e902bd83927f27e36ae2b19f6b3e9
Dave Kinghorn <davidreact@gmail.com>
2022-02-22 16:42:27 -0500

    Consistency fix

--- src/_main_/utils/emailer/send_email.py
+++ src/_main_/utils/emailer/send_email.py
@@ -90,13 +90,13 @@ def old_send_massenergize_mass_email(subject, msg, recipient_emails):
 
   return True
 
-def send_massenergize_mass_email(subject, msg, recipient_emails):
+def send_massenergize_mass_email(subject, msg, to):
   message = pystmark.Message(
     subject=subject,
+    to=to,
     sender=FROM_EMAIL, 
     html=msg, 
   )
-  message.recipients = recipient_emails
   response = pystmark.send(message, api_key=EMAIL_POSTMARK_SERVER_TOKEN)
 
   if not response.ok:
