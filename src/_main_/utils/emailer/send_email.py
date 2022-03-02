ca13b88fe71e15ba329b9b4accaad78d1d95fb16
Dave Kinghorn <davidreact@gmail.com>
2022-03-02 15:12:48 -0500

    Postmark template exploration

--- src/_main_/utils/emailer/send_email.py
+++ src/_main_/utils/emailer/send_email.py
@@ -3,10 +3,26 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from sentry_sdk import capture_message
 import pystmark
+import requests
 
 from _main_.settings import EMAIL_POSTMARK_SERVER_TOKEN
+from _main_.settings import EMAIL_POSTMARK_API_ENDPOINT_URL
 
 FROM_EMAIL = 'no-reply@massenergize.org'
+
+def get_email_templates():
+  request = requests.get("{}/templates".format(EMAIL_POSTMARK_API_ENDPOINT_URL),headers={
+    "Accept": "application/json",
+    "X-Postmark-Server-Token": EMAIL_POSTMARK_SERVER_TOKEN
+  }, params={
+    "Count": 10,
+    "Offset": 0
+  }
+  )
+
+  return request.json()
+
+templates = get_email_templates()
 
 def old_send_massenergize_email(subject, msg, to):
   ok = send_mail(
@@ -52,28 +68,28 @@ def old_send_massenergize_rich_email(subject, to, massenergize_email_type, conte
     return False
   return True
 
+# def send_massenergize_rich_email(subject, to, massenergize_email_type, content_variables, from_email=None):
+#   if not from_email:
+#     from_email = FROM_EMAIL
+#   html_content = render_to_string(massenergize_email_type, content_variables)
+#   text_content = strip_tags(html_content)
+
+#   message = pystmark.Message(
+#     subject=subject,
+#     to=to,
+#     sender=from_email, 
+#     html=html_content, 
+#     text=text_content
+#   )
+#   response = pystmark.send(message, api_key=EMAIL_POSTMARK_SERVER_TOKEN)
+
+#   if not response.ok:
+#     capture_message(f"Error Occurred in Sending Email to {to}", level="error")
+#     print(response.raise_for_status())
+#     return False
+#   return True
+
 def send_massenergize_rich_email(subject, to, massenergize_email_type, content_variables, from_email=None):
-  if not from_email:
-    from_email = FROM_EMAIL
-  html_content = render_to_string(massenergize_email_type, content_variables)
-  text_content = strip_tags(html_content)
-
-  message = pystmark.Message(
-    subject=subject,
-    to=to,
-    sender=from_email, 
-    html=html_content, 
-    text=text_content
-  )
-  response = pystmark.send(message, api_key=EMAIL_POSTMARK_SERVER_TOKEN)
-
-  if not response.ok:
-    capture_message(f"Error Occurred in Sending Email to {to}", level="error")
-    print(response.raise_for_status())
-    return False
-  return True
-
-def send_massenergize_email_template(subject, to, massenergize_email_type, content_variables, from_email=None):
   if not from_email:
     from_email = FROM_EMAIL
 
@@ -88,12 +104,12 @@ def send_massenergize_email_template(subject, to, massenergize_email_type, conte
   template_id_welocme = 27142713
 
   message = pystmark.Message(
-    subject=subject,
     to=to,
     sender=from_email, 
     template_id=template_id_welocme,
     template_model=t_model
   )
+
   response = pystmark.send_with_template(message, api_key=EMAIL_POSTMARK_SERVER_TOKEN)
 
   if not response.ok:
@@ -134,3 +150,6 @@ def send_massenergize_mass_email(subject, msg, to):
     return False
 
   return True
+
+
+get_email_templates()
