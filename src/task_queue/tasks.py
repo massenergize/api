81e02d664075d40a72f2507582967ecdd8c99538
Tahiru <abdullai.tahiru@gmail.com>
2022-03-24 08:47:57 +0000

    max retrys

--- src/task_queue/tasks.py
+++ src/task_queue/tasks.py
@@ -4,7 +4,7 @@ from .models import Task
 from .type_constants import TaskStatus
 
 
-@shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={'max_retries': 3, 'countdown': 5})
+@shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={'max_retries': 2, 'countdown': 5})
 def run_some_task(self, task_id):
     task = Task.objects.get(id=task_id)
     func = FUNCTIONS.get(task.job_name)
