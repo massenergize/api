1846e04e85ee98610e84e0e6524f0542992c1355
Tahiru <abdullai.tahiru@gmail.com>
2022-04-29 22:01:30 +0000

    getting weekly data

--- src/api/tasks.py
+++ src/api/tasks.py
@@ -9,7 +9,8 @@ from celery import shared_task
 from api.store.download import DownloadStore
 from database.models import Community, CommunityMember, UserActionRel, UserProfile
 from django.utils import timezone
-from django.utils.dateparse import parse_datetime
+import datetime
+from django.utils.timezone import utc
 
 
 def generate_csv_and_email(data, download_type, community_name=None, email=None):
@@ -90,31 +91,16 @@ def download_data(self, args, download_type):
 
 @shared_task(bind=True)
 def generate_and_send_weekly_digest_to_cadmins(self):
-    today = timezone.now().date()
+    today = datetime.datetime.utcnow().replace(tzinfo=utc)
     one_week_ago = today - timezone.timedelta(days=7)
-
-    print("================== Generating weekly digest...", one_week_ago, "==================/n/n")
 
     all_super_admins= UserProfile.objects.filter(is_super_admin=True)
     live_communities = Community.objects.filter(is_approved=True).count()
     drafted_communities = Community.objects.filter(is_approved=False).count()
-    communities_and_number_of_weekly_signups = CommunityMember.objects.filter(community__is_approved=True, created_at__gte=one_week_ago).values(
-        'community__name').annotate(signups=Count("community")).order_by('community')
-    actions_taken_by_each_community = UserActionRel.objects.exclude(status='SAVE_FOR_LATER').values('action__community__name').annotate(actions=Count("action__community")).order_by('action__community')
+    communities_and_number_of_weekly_signups = CommunityMember.objects.filter(community__is_approved=True, created_at__gte=one_week_ago).values('community__name').annotate(signups=Count("community")).order_by('community')
+    actions_taken_by_each_community = UserActionRel.objects.filter(created_at__gte=one_week_ago).exclude(status='SAVE_FOR_LATER').values(
+        'action__community__name', 'carbon_impact').annotate(actions=Count("action__community")).order_by('action__community')
 
-
-    print('+++++++++++++++++++++ Data +++++++++++++++++')
-    print(f'Number of live communities: {live_communities}\n')
-    print(live_communities)
-    print(f'\n Number of drafted communities: {drafted_communities}\n')
-    print(drafted_communities)
-    print(
-        f'\n Number Weekly signups by each community: {len(communities_and_number_of_weekly_signups)}\n')
-    print(communities_and_number_of_weekly_signups)
-    print(
-        f'\n Number of actions taken by each community: {len(actions_taken_by_each_community)}\n')
-    print(actions_taken_by_each_community)
-    print('\n\n+++++++++++++++++++++ res +++++++++++++++++')
 
 
     return "success"
