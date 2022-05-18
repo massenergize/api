40f8d40bedd027ec045c6544ce51fd5347d1ba3f
Tahiru <abdullai.tahiru@gmail.com>
2022-05-18 21:07:14 +0000

    getting only replies that are not archived

--- src/database/models.py
+++ src/database/models.py
@@ -3093,7 +3093,7 @@ class Message(models.Model):
         res["community"] = get_summary_info(self.community)
         res["team"] = get_summary_info(self.team)
         res["user"] = get_summary_info(self.user)
-        res["replies"] = [r.simple_json() for r in Message.objects.filter(parent=self)]
+        res["replies"] = [r.simple_json() for r in Message.objects.filter(parent=self, archived=False)]
         res["created_at"] = self.created_at.strftime("%Y-%m-%d %H:%M")
         return res
 
