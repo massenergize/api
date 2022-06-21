271c0ab09637e20550f1a1ba6c6777ddf2fd1237
frimpongopoku <pongofrimi@Gmail.com>
2022-06-21 13:35:37 +0400

    tweaks to settings default for user portal

--- src/database/utils/settings/user_settings.py
+++ src/database/utils/settings/user_settings.py
@@ -23,69 +23,90 @@ class UserPortalSettings:
 
     Settings = {
         "general_settings": {
-            "update_frequency": {
-                "type": "radio",
-                "text": "How frequently would you like to receive notifications from us about new events, actions, or financial incentives?",
-                "values": common_options,
-            },
-            "news_letter": {
-                "type": "radio",
-                "text": "Send me news  updates in my community",
-                "values": {
-                    "as_posted": {"name": "As posted", "value": False},
-                    "never": {"name": "Never", "value": False},
+            "name": "General Settings",
+            "live": True,
+            "options": {
+                "testing_Checkboxes": {
+                    "type": "checkbox",
+                    "text": "This is a question that requires a checkbox?",
+                    "values": common_options,
                 },
-            },
-            "messaging": {
-                "type": "radio",
-                "text": "Would you like to receive messages from other members of the community?",
-                "values": {
-                    "yes": {"name": "Yes", "value": False},
-                    "from_team": {"name": "Only from those on my team", "value": False},
-                    "no": {"name": "No", "value": False},
+                "update_frequency": {
+                    "type": "radio",
+                    "text": "How frequently would you like to receive notifications from us about new events, actions, or financial incentives?",
+                    "values": common_options,
+                },
+                "news_letter": {
+                    "type": "radio",
+                    "text": "Send me news  updates in my community",
+                    "values": {
+                        "as_posted": {"name": "As posted", "value": False},
+                        "never": {"name": "Never", "value": False},
+                    },
+                },
+                "messaging": {
+                    "type": "radio",
+                    "text": "Would you like to receive messages from other members of the community?",
+                    "values": {
+                        "yes": {"name": "Yes", "value": False},
+                        "from_team": {
+                            "name": "Only from those on my team",
+                            "value": False,
+                        },
+                        "no": {"name": "No", "value": False},
+                    },
                 },
             },
         },
         "advanced_settings": {
-            "upcoming_events": {
-                "type": "radio",
-                "text": "Send me information about events coming up",
-                "values": common_options,
-            },
-            "upcoming_actions": {
-                "type": "radio",
-                "text": "Send me information about new actions",
-                "values": common_options,
-            },
-            "new_teams": {
-                "type": "radio",
-                "text": "Send me information about new teams created",
-                "values": common_options,
-            },
-            "new_testimonials": {
-                "type": "radio",
-                "text": "Send me information about new testimonials regarding actions in my to do list",
-                "values": common_options,
-            },
-            "your_activity_updates": {
-                "type": "radio",
-                "text": "Send me emails based on my activity (or non-activity) on the MassEnergize website",
-                "values": common_options,
+            "name": "Advanced Settings",
+            "live": True,
+            "options": {
+                "upcoming_events": {
+                    "live": False,
+                    "type": "radio",
+                    "text": "Send me information about events coming up",
+                    "values": common_options,
+                },
+                "upcoming_actions": {
+                    "live": False,
+                    "type": "radio",
+                    "text": "Send me information about new actions",
+                    "values": common_options,
+                },
+                "new_teams": {
+                    "live": False,
+                    "type": "radio",
+                    "text": "Send me information about new teams created",
+                    "values": common_options,
+                },
+                "new_testimonials": {
+                    "live": False,
+                    "type": "radio",
+                    "text": "Send me information about new testimonials regarding actions in my to do list",
+                    "values": common_options,
+                },
+                "your_activity_updates": {
+                    "live": False,
+                    "type": "radio",
+                    "text": "Send me emails based on my activity (or non-activity) on the MassEnergize website",
+                    "values": common_options,
+                },
             },
         },
     }
 
     Defaults = {
         "general_settings": {
-            "update_frequency": {"values": {"per_week": True}},
-            "news_letter": {"values": {"as_posted": True}},
-            "messaging": {"values": {"yes": True}},
+            "update_frequency": {"per_week": {"value": True}},
+            "news_letter": {"as_posted": {"value": True}},
+            "messaging": {"yes": {"value": True}},
         },
         "advanced_settings": {
-            "upcoming_events": {"values": {"never": True}},
-            "upcoming_actions": {"values": {"never": True}},
-            "news_teams": {"values": {"never": True}},
-            "new_testimonials": {"values": {"never": True}},
-            "your_activity_updates": {"values": {"never": True}},
+            "upcoming_events": {"never": {"value": True}},
+            "upcoming_actions": {"never": {"value": True}},
+            "news_teams": {"never": {"value": True}},
+            "new_testimonials": {"never": {"value": True}},
+            "your_activity_updates": {"never": {"value": True}},
         },
     }
