c05fbc2793a226dd6a99825ad3860b6792d36af4
Tahiru <abdullai.tahiru@gmail.com>
2022-07-01 23:47:05 +0000

    making the changes

--- src/api/tasks.py
+++ src/api/tasks.py
@@ -7,7 +7,7 @@ from api.constants import ACTIONS, COMMUNITIES, METRICS, TEAMS, USERS
 from api.store.download import DownloadStore
 from celery import shared_task
 from api.store.download import DownloadStore
-from api.utils.constants import CADMIN_EMAIL_TEMPLATE_ID, SADMIN_EMAIL_TEMPLATE_ID
+from api.utils.constants import CADMIN_EMAIL_TEMPLATE_ID, DATA_DOWNLOAD_TEMPLATE_ID, SADMIN_EMAIL_TEMPLATE_ID
 from database.models import Community, CommunityAdminGroup, CommunityMember, UserActionRel, UserProfile
 from django.utils import timezone
 import datetime
@@ -23,7 +23,7 @@ def generate_csv_and_email(data, download_type, community_name=None, email=None)
     writer = csv.writer(response)
     for row in data:
         writer.writerow(row)
-    send_massenergize_email_with_attachments(f"{download_type.capitalize()} Data", f"Here is the {download_type} data you requested. Please see the attachment for details", email, response.content, filename)
+    send_massenergize_email_with_attachments(DATA_DOWNLOAD_TEMPLATE_ID,temp_data,email, response.content, filename)
     return True
 
 
