387c01c8395b065ec119bd10254bbf3e235266fd
frimpongopoku <pongofrimi@Gmail.com>
2022-07-07 12:56:44 +0400

    introduced add and update routes for feature flags

--- src/api/handlers/feature_flag.py
+++ src/api/handlers/feature_flag.py
@@ -6,7 +6,6 @@ from _main_.utils.massenergize_response import MassenergizeResponse
 from _main_.utils.context import Context
 
 
-
 class FeatureFlagHandler(RouteHandler):
     def __init__(self):
         super().__init__()
@@ -16,6 +15,48 @@ class FeatureFlagHandler(RouteHandler):
     def registerRoutes(self) -> None:
         self.add("/featureFlags.info", self.feature_flag_info)
         self.add("/featureFlags.list", self.feature_flags)
+        self.add("/featureFlags.add", self.add_feature_flag)
+        self.add("/featureFlags.info.update", self.update_feature_flag)
+
+    def add_feature_flag(self, request):
+        context: Context = request.context
+        args: dict = context.args
+        self.validator.expect("name", str, is_required=True).expect(
+            "expires_on", str, is_required=True
+        ).expect("community_ids", "str_list").expect("notes", str).expect(
+            "user_ids", "str_list"
+        ).expect(
+            "owner", str
+        ).expect(
+            "audience", str, is_required=True
+        ).expect(
+            "user_audience", str, is_required=True
+        ).expect(
+            "key", str, is_required=True
+        )
+        args, err = self.validator.verify(args, strict=True)
+        if err:
+            return err
+
+        data, err = self.service.add_feature_flag(context, args)
+        if err:
+            return err
+        return MassenergizeResponse(data=data)
+
+    def update_feature_flag(self, request):
+        context: Context = request.context
+        args: dict = context.args
+        self.validator.expect("id", int, is_required=True).expect(
+            "community_ids", "str_list"
+        ).expect("notes", str).expect("user_ids", "str_list")
+        args, err = self.validator.verify(args)
+        if err:
+            return err
+
+        data, err = self.service.update_feature_flag(context, args)
+        if err:
+            return err
+        return MassenergizeResponse(data=data)
 
     def feature_flag_info(self, request):
         context: Context = request.context
--- src/api/store/feature_flag.py
+++ src/api/store/feature_flag.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 from _main_.utils.context import Context
 from django.db.models import Q
@@ -7,9 +6,18 @@ from _main_.utils.massenergize_errors import (
     InvalidResourceError,
     MassEnergizeAPIError,
 )
-from database.models import FeatureFlag
+from _main_.utils.utils import Console
+from database.models import Community, FeatureFlag, UserProfile
 
-from .utils import find_reu_community, get_community, get_community_or_die, get_user, get_user_or_die, split_location_string, check_location
+from .utils import (
+    find_reu_community,
+    get_community,
+    get_community_or_die,
+    get_user,
+    get_user_or_die,
+    split_location_string,
+    check_location,
+)
 from sentry_sdk import capture_message
 from typing import Tuple
 
@@ -18,7 +26,9 @@ class FeatureFlagStore:
     def __init__(self):
         self.name = "Miscellaneous Store/DB"
 
-    def get_feature_flag_info(self, ctx: Context, feature_flag_id) -> Tuple[dict, MassEnergizeAPIError]:
+    def get_feature_flag_info(
+        self, ctx: Context, feature_flag_id
+    ) -> Tuple[dict, MassEnergizeAPIError]:
         try:
             ff = FeatureFlag.objects.get(id=feature_flag_id)
             if not ff:
@@ -28,19 +38,72 @@ class FeatureFlagStore:
             capture_message(str(e), level="error")
             return None, CustomMassenergizeError(e)
 
+    def add_feature_flag(self, ctx: Context, args) -> Tuple[dict, MassEnergizeAPIError]:
+        try:
+            com_ids = args.pop("community_ids", [])
+            user_ids = args.pop("user_ids", [])
+            communities = Community.objects.filter(id__in=com_ids) if com_ids else None
+            users = UserProfile.objects.filter(id=user_ids) if user_ids else None
+            flag = FeatureFlag(**args)
+            flag.save()
+            if communities:
+                flag.communities.set(communities)
+            if users:
+                flag.users.set(users)
+            return flag, None
 
-    def get_feature_flags(self, ctx: Context, args: dict) -> Tuple[dict, MassEnergizeAPIError]:
+        except Exception as e:
+            capture_message(str(e), level="error")
+            return None, CustomMassenergizeError(e)
+
+    def update_feature_flag(
+        self, ctx: Context, args
+    ) -> Tuple[dict, MassEnergizeAPIError]:
+        try:
+            id = args.pop("id", None)
+            com_ids = args.pop("community_ids", [])
+            user_ids = args.pop("user_ids", [])
+            found = FeatureFlag.objects.filter(id=id)
+            if not found:
+                return None, CustomMassenergizeError(
+                    "Sorry, could not find the feature you want to update"
+                )
+            communities = Community.objects.filter(id__in=com_ids) if com_ids else None
+            users = UserProfile.objects.filter(id=user_ids) if user_ids else None
+            found.update(**args)
+            flag = found.first()
+            if communities:
+                flag.communities.clear()
+                flag.communities.set(communities)
+            if users:
+                flag.users.clear()
+                flag.users.set(users)
+            return flag, None
+
+        except Exception as e:
+            capture_message(str(e), level="error")
+            return None, CustomMassenergizeError(e)
+
+    def get_feature_flags(
+        self, ctx: Context, args: dict
+    ) -> Tuple[dict, MassEnergizeAPIError]:
         try:
 
             # first get the un-expired feature flags that are turned on for everyone
-            ff = FeatureFlag.objects.filter(expires_on__gt=datetime.now(), on_for_everyone=True) # if it is turned on for everyone we want it
-            
+            ff = FeatureFlag.objects.filter(
+                expires_on__gt=datetime.now(), on_for_everyone=True
+            )  # if it is turned on for everyone we want it
+
             # if community is found, fetch the feature flags turned ON specifically for this community
-            community,_ = get_community(args.get('community_id'), args.get('subdomain'))
+            community, _ = get_community(
+                args.get("community_id"), args.get("subdomain")
+            )
             if community:
-                ff |= community.community_feature_flags.filter(expires_on__gt=datetime.now())
-            
-            user,_ = get_user(ctx.user_id, ctx.user_email)
+                ff |= community.community_feature_flags.filter(
+                    expires_on__gt=datetime.now()
+                )
+
+            user, _ = get_user(ctx.user_id, ctx.user_email)
             if user:
                 ff |= user.user_feature_flags.filter(expires_on__gt=datetime.now())
 
