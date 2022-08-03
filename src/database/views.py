705c1ecaad3eaf89b8097946c81281daf65a77ce
Mattia Danese <mattiadanese@Mattias-MacBook-Air.local>
2022-08-03 16:44:00 -0400

    views.py cleanup

--- src/database/views.py
+++ src/database/views.py
@@ -9,11 +9,11 @@ from database.models import *
 
 
 def make_UserMediaUpload_for_every_Media(self, request, qs):
-    # make_UserMediaUpload_for_Communities()
+    make_UserMediaUpload_for_Communities()
     make_UserMediaUpload_for_Actions_Events_Testimonials(Action.objects.values_list('community', 'image', 'user'), "action")
     make_UserMediaUpload_for_Actions_Events_Testimonials(Event.objects.values_list('community', 'image', 'user'), "event")
     make_UserMediaUpload_for_Actions_Events_Testimonials(Testimonial.objects.values_list('community', 'image', 'user'), "testimonial")
-    # make_UserMediaUpload_for_Vendors()
+    make_UserMediaUpload_for_Vendors()
     
 
 def make_UserMediaUpload_for_Communities():
@@ -119,7 +119,7 @@ def make_UserMediaUpload_for_Actions_Events_Testimonials(all_media, type):
 
 def make_UserMediaUpload_for_Vendors():
     vendor_media = Vendor.objects.values_list('communities', 'logo', 'banner', 'user')
-    print("vendor media:", list(vendor_media))
+    # print("vendor media:", list(vendor_media))
 
     for comm_id, logo, banner, user in vendor_media:
         community = Community.objects.get(id = comm_id) if comm_id else None
@@ -171,41 +171,3 @@ def make_UserMediaUpload_for_Vendors():
 
             if community:
                 new_media.communities.ad
-        
-
-
-
-# MEDIA
-# id = models.AutoField(primary_key=True)
-# name = models.SlugField(max_length=SHORT_STR_LEN, blank=True)
-# file = models.FileField(upload_to="media/")
-# media_type = models.CharField(max_length=SHORT_STR_LEN, blank=True)
-# is_deleted = models.BooleanField(default=False, blank=True)
-# order = models.PositiveIntegerField(default=0, blank=True, null=True)
-
-
-
-# USER MEDIA UPLOAD:
-# id = models.AutoField(primary_key=True)
-# user = models.ForeignKey(
-#     UserProfile,
-#     null=True,
-#     related_name="uploads",
-#     on_delete=models.DO_NOTHING,
-# )
-# communities = models.ManyToManyField(
-#     Community,
-#     related_name="community_uploads",
-# )
-# media = models.OneToOneField(
-#     Media,
-#     null=True,
-#     related_name="user_upload",
-#     on_delete=models.CASCADE,
-# )
-# is_universal = BooleanField(
-#     default=False
-# )  # True value here means image is available to EVERYONE, and EVERY COMMUNITY
-# settings = models.JSONField(null=True, blank=True)
-# created_at = models.DateTimeField(auto_now_add=True)
-# updated_at = models.DateTimeField(auto_now=True)
