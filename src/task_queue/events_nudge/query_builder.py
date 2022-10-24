cc3e8ad5a33418c8490ebf4f5a671118203c2c75
Tahiru <abdullai.tahiru@gmail.com>
2022-10-24 22:18:07 +0000

    Weekly event report nudge complete

--- src/task_queue/events_nudge/query_builder.py
+++ src/task_queue/events_nudge/query_builder.py
@@ -1,10 +1,9 @@
 from _main_.utils.common import serialize_all
-from _main_.utils.emailer.send_email import send_massenergize_email_with_attachments
-from api.utils.constants import WEEKLY_EVENTS_NUDGE_TEMPLATE_ID
 from database.models import *
 from django.utils import timezone
 import datetime
 from django.utils.timezone import utc
+from _main_.settings import DJANGO_ENV
 
 
 today = datetime.datetime.utcnow().replace(tzinfo=utc)
@@ -17,9 +16,7 @@ def is_viable(item):
             return True
     return False
 
-def get_events_created_within_the_week():
-    events = Event.objects.filter(live_at__gte=one_week_ago, live_at__lte=today, is_deleted=False)
-    return serialize_all(events)
+
 
 def get_comm_emails_and_pref():
     communities = Community.objects.all()
@@ -48,20 +45,12 @@ def get_sadmin_emails_and_pre():
     return sadmins
 
 
-def remove_dups():
+def remove_admin_duplicates():
     sadmins = get_sadmin_emails_and_pre()
     cadmins = get_comm_emails_and_pref()
-
     all = sadmins+cadmins
-    result = list(
-    {
-        dictionary['email']: dictionary
-        for dictionary in all
-    }.values()
-)
-
-
-    return result
+    no_duplicates = list({dictionary['email']: dictionary for dictionary in all }.values())
+    return no_duplicates
 
 
 def human_readable_date(start, end):
@@ -70,24 +59,50 @@ def human_readable_date(start, end):
     else:
         return start.strftime("%b %d, %H:%M") +" - "+ end.strftime( "%b %d, %H:%M")
 
-def test_email():
-    all= remove_dups()
-    email_list = [i.get("email") for i in all]
-    events = get_events_created_within_the_week()
-    if events==[] or events ==None:
-        return
-    t_model={
+
+
+def generate_redirect_url(id):
+    domain = ''
+
+    if DJANGO_ENV == "local":
+        domain = 'http://localhost:3001/'
+    elif DJANGO_ENV == "canary":
+        domain ="https://admin-canary.massenergize.org/"
+    else:
+        domain = "https://admin.massenergize.org/"
+
+    return domain + f"admin/read/event/{id}/event-view"
+
+
+def get_email_list(frequency):
+    admins = remove_admin_duplicates()
+    email_list = []
+    for admin in admins:
+        admin_settings = admin.get("pref", {}).get("admin_portal_settings", {}).get("general_settings")
+        freq = admin_settings.get("notifications")
+        if freq.get(frequency).get("value") ==True:
+            email_list.append(admin.get("email"))
+
+    return email_list
+
+
+
+
+
+
+def get_live_events_within_the_week():
+    events_query = Event.objects.filter(live_at__gte=one_week_ago, live_at__lte=today, is_deleted=False) 
+    events = serialize_all(events_query, full=True)
+    data={
         "events":[{
             "title": event.get("name"),
             "community": event.get("community",{}).get("name") if event.get("community") else "N/A",
             "period":human_readable_date(event.get("start_date_and_time"), event.get("end_date_and_time")),
             "location":"Online" if event.get("location") else "In person",
-            "id":event.get("id"),
-            "link":"https://www.geeksforgeeks.org/python-datetime-module/"
+            "link":generate_redirect_url(event.get("id"))
         } for event in events]
     }
-    send_massenergize_email_with_attachments(WEEKLY_EVENTS_NUDGE_TEMPLATE_ID, t_model, email_list, None, None)
+    return data
+ 
+ 
 
-
-# construct a link to view event details
-# construct a link to copy event to community :: hide this if user is from host community
--- src/task_queue/tasks.py
+++ src/task_queue/tasks.py
@@ -1,7 +1,15 @@
 from celery import shared_task
+
+from task_queue.events_nudge.query_builder import get_email_list, get_live_events_within_the_week
 from .jobs import FUNCTIONS
 from .models import Task
 
+from _main_.utils.emailer.send_email import send_massenergize_email_with_attachments
+from api.utils.constants import WEEKLY_EVENTS_NUDGE_TEMPLATE_ID
+
+WEEKLY= "weekly"
+BI_WEEKLY = "bi-weekly"
+MONTHLY = "monthly"
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={'max_retries': 2, 'countdown': 5})
 def run_some_task(self, task_id):
@@ -18,3 +26,10 @@ def run_some_task(self, task_id):
     task.save()
 
 
+@shared_task(bind=True)
+def send_weekly_events_report(self):
+    """
+    """
+    email_list =  get_email_list(WEEKLY) 
+    data = get_live_events_within_the_week()
+    send_massenergize_email_with_attachments(WEEKLY_EVENTS_NUDGE_TEMPLATE_ID, data, email_list, None, None)
