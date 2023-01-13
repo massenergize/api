a7ca560c7b6e3d819281fd0135847330957778bf
tahiru <abdullai.tahiru@gmail.com>
2023-01-13 21:10:34 +0000

    code cleanup

--- src/task_queue/tasks.py
+++ src/task_queue/tasks.py
@@ -8,11 +8,11 @@ WEEKLY= "weekly"
 BI_WEEKLY = "bi-weekly"
 MONTHLY = "monthly"
 
-
+today = datetime.date.today()
 @shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={'max_retries': 2, 'countdown': 5})
 def run_some_task(self, task_id):
     task = Task.objects.get(id=task_id)
-    if(task.last_run == None or task.last_run != datetime.date.today()):
+    if(task.last_run == None or task.last_run != today):
         func = FUNCTIONS.get(task.job_name)
         if func:
             task.status = "SCHEDULED"
@@ -21,6 +21,6 @@ def run_some_task(self, task_id):
             task.status = "SUCCEEDED" if res else "FAILED"
         else:
             task.status = "FAILED"
-        task.last_run = datetime.datetime.now()
+        task.last_run = today
         task.save()
  
