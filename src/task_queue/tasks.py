8a2bf6ee35b279dce34224323ee3674f435cff73
tahiru <abdullai.tahiru@gmail.com>
2023-01-13 20:54:02 +0000

    celery prod duplication fix

--- src/task_queue/tasks.py
+++ src/task_queue/tasks.py
@@ -1,3 +1,4 @@
+import datetime
 from celery import shared_task
 from .jobs import FUNCTIONS
 from .models import Task
@@ -7,17 +8,19 @@ WEEKLY= "weekly"
 BI_WEEKLY = "bi-weekly"
 MONTHLY = "monthly"
 
+
 @shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={'max_retries': 2, 'countdown': 5})
 def run_some_task(self, task_id):
     task = Task.objects.get(id=task_id)
-    func = FUNCTIONS.get(task.job_name)
-    if func:
-        task.status = "SCHEDULED"
+    if(task.last_run == None or task.last_run != datetime.date.today()):
+        func = FUNCTIONS.get(task.job_name)
+        if func:
+            task.status = "SCHEDULED"
+            task.save()
+            res = func()
+            task.status = "SUCCEEDED" if res else "FAILED"
+        else:
+            task.status = "FAILED"
+        task.last_run = datetime.datetime.now()
         task.save()
-        res = func()
-        task.status = "SUCCEEDED" if res else "FAILED"
-    else:
-        task.status = "FAILED"
-
-    task.save()
  
