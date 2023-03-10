dbd1bb82cafbacf88e622aea2bd2cbc59956973d
frimpongopoku <pongofrimi@Gmail.com>
2023-03-10 09:31:51 +0400

    added fxn that finds admins and sends them mou email

--- src/task_queue/views.py
+++ src/task_queue/views.py
@@ -7,30 +7,62 @@ from django.utils import timezone
 import datetime
 from django.utils.timezone import utc
 from django.db.models import Count
+from django.core.exceptions import ObjectDoesNotExist
+
 
 today = datetime.datetime.utcnow().replace(tzinfo=utc)
 one_week_ago = today - timezone.timedelta(days=7)
-communities = Community.objects.all().order_by('is_approved')
+communities = Community.objects.all().order_by("is_approved")
+
 
 def query_db():
     """
     Query data from the database.
     """
-    communities_total_signup = CommunityMember.objects.filter(community__is_approved=True).values('community__name').annotate(signups=Count("community")).order_by('community')
-    communities_weekly_signup = CommunityMember.objects.filter(community__is_approved=True, created_at__gte=one_week_ago).values('community__name').annotate(signups=Count("community")).order_by('community')
-    communities_total_actions = UserActionRel.objects.filter(created_at__gte=one_week_ago).exclude(status='SAVE_FOR_LATER').values('action__community__name', 'carbon_impact').annotate(actions=Count("action__community")).order_by('action__community')
-    communities_weekly_done_actions = UserActionRel.objects.filter(created_at__gte=one_week_ago, created_at__lte=today, status='DONE').values('action__community__name', 'carbon_impact').annotate(actions=Count("action__community")).order_by('action__community')
-    communities_weekly_todo_actions = UserActionRel.objects.filter(created_at__gte=one_week_ago, created_at__lte=today, status="TODO").values('action__community__name', 'carbon_impact').annotate(actions=Count("action__community")).order_by('action__community')
+    communities_total_signup = (
+        CommunityMember.objects.filter(community__is_approved=True)
+        .values("community__name")
+        .annotate(signups=Count("community"))
+        .order_by("community")
+    )
+    communities_weekly_signup = (
+        CommunityMember.objects.filter(
+            community__is_approved=True, created_at__gte=one_week_ago
+        )
+        .values("community__name")
+        .annotate(signups=Count("community"))
+        .order_by("community")
+    )
+    communities_total_actions = (
+        UserActionRel.objects.filter(created_at__gte=one_week_ago)
+        .exclude(status="SAVE_FOR_LATER")
+        .values("action__community__name", "carbon_impact")
+        .annotate(actions=Count("action__community"))
+        .order_by("action__community")
+    )
+    communities_weekly_done_actions = (
+        UserActionRel.objects.filter(
+            created_at__gte=one_week_ago, created_at__lte=today, status="DONE"
+        )
+        .values("action__community__name", "carbon_impact")
+        .annotate(actions=Count("action__community"))
+        .order_by("action__community")
+    )
+    communities_weekly_todo_actions = (
+        UserActionRel.objects.filter(
+            created_at__gte=one_week_ago, created_at__lte=today, status="TODO"
+        )
+        .values("action__community__name", "carbon_impact")
+        .annotate(actions=Count("action__community"))
+        .order_by("action__community")
+    )
     return {
-        'total_sign_ups': communities_total_signup,
-        'weekly_sign_ups': communities_weekly_signup,
-        'total_actions': communities_total_actions,
-        'weekly_done_actions': communities_weekly_done_actions,
-        'weekly_todo_actions': communities_weekly_todo_actions,
-     }
-
-
-
+        "total_sign_ups": communities_total_signup,
+        "weekly_sign_ups": communities_weekly_signup,
+        "total_actions": communities_total_actions,
+        "weekly_done_actions": communities_weekly_done_actions,
+        "weekly_todo_actions": communities_weekly_todo_actions,
+    }
 
 
 def super_admin_nudge():
@@ -39,44 +71,89 @@ def super_admin_nudge():
     """
     response = HttpResponse(content_type="text/csv")
     writer = csv.writer(response)
-    writer.writerow(['Community','Total Signups', 'Signups This Week', 'Total Actions Taken', ' Actions Taken This Week ', 'Actions in ToDo This Week'])
+    writer.writerow(
+        [
+            "Community",
+            "Total Signups",
+            "Signups This Week",
+            "Total Actions Taken",
+            " Actions Taken This Week ",
+            "Actions in ToDo This Week",
+        ]
+    )
 
     data = query_db()
 
-    super_admins = UserProfile.objects.filter(is_super_admin=True, is_deleted=False).values_list("email", flat=True)
+    super_admins = UserProfile.objects.filter(
+        is_super_admin=True, is_deleted=False
+    ).values_list("email", flat=True)
 
     for community in communities:
         community_name = community.name
-        all_community_admins = CommunityAdminGroup.objects.filter(community=community).values_list('members__email', flat=True)
+        all_community_admins = CommunityAdminGroup.objects.filter(
+            community=community
+        ).values_list("members__email", flat=True)
         all_community_admins = list(all_community_admins)
 
-        total_signup  = data.get('total_sign_ups').filter(community__name=community_name).first()
-        community_total_signup = total_signup['signups'] if total_signup else 0
+        total_signup = (
+            data.get("total_sign_ups").filter(community__name=community_name).first()
+        )
+        community_total_signup = total_signup["signups"] if total_signup else 0
 
-        weekly_signup = data.get('weekly_sign_ups').filter(community__name=community_name).first()
-        community_weekly_signup = weekly_signup['signups'] if weekly_signup else 0
+        weekly_signup = (
+            data.get("weekly_sign_ups").filter(community__name=community_name).first()
+        )
+        community_weekly_signup = weekly_signup["signups"] if weekly_signup else 0
 
-        total_actions = data.get('total_actions').filter(action__community__name=community_name).first()
-        community_actions_taken = total_actions['actions'] if total_actions else 0
+        total_actions = (
+            data.get("total_actions")
+            .filter(action__community__name=community_name)
+            .first()
+        )
+        community_actions_taken = total_actions["actions"] if total_actions else 0
 
-        weekly_done_actions = data.get('weekly_done_actions').filter(action__community__name=community_name).first()
-        community_weekly_done_actions = weekly_done_actions['actions'] if weekly_done_actions else 0
+        weekly_done_actions = (
+            data.get("weekly_done_actions")
+            .filter(action__community__name=community_name)
+            .first()
+        )
+        community_weekly_done_actions = (
+            weekly_done_actions["actions"] if weekly_done_actions else 0
+        )
 
-        weekly_todo_actions = data.get('weekly_todo_actions').filter(action__community__name=community_name).first()
-        community_weekly_todo_actions = weekly_todo_actions['actions'] if weekly_todo_actions else 0
-    
+        weekly_todo_actions = (
+            data.get("weekly_todo_actions")
+            .filter(action__community__name=community_name)
+            .first()
+        )
+        community_weekly_todo_actions = (
+            weekly_todo_actions["actions"] if weekly_todo_actions else 0
+        )
 
-        writer.writerow([community_name, community_total_signup,community_weekly_signup, community_actions_taken, community_weekly_done_actions, community_weekly_todo_actions])
-    temp_data =  {
-            'name':"there",
-            'start': str(one_week_ago.date()),
-            'end': str(today.date()),
-        }
+        writer.writerow(
+            [
+                community_name,
+                community_total_signup,
+                community_weekly_signup,
+                community_actions_taken,
+                community_weekly_done_actions,
+                community_weekly_todo_actions,
+            ]
+        )
+    temp_data = {
+        "name": "there",
+        "start": str(one_week_ago.date()),
+        "end": str(today.date()),
+    }
 
-    send_nudge(response.content, f'Weekly Report({one_week_ago.date()} to {today.date()}).csv',list(super_admins), SADMIN_EMAIL_TEMPLATE_ID,temp_data )
+    send_nudge(
+        response.content,
+        f"Weekly Report({one_week_ago.date()} to {today.date()}).csv",
+        list(super_admins),
+        SADMIN_EMAIL_TEMPLATE_ID,
+        temp_data,
+    )
     return "success"
-
-
 
 
 def community_admin_nudge():
@@ -85,35 +162,123 @@ def community_admin_nudge():
     for community in communities:
         community_name = community.name
 
-        all_community_admins = CommunityAdminGroup.objects.filter(community=community).values_list('members__email', flat=True)
+        all_community_admins = CommunityAdminGroup.objects.filter(
+            community=community
+        ).values_list("members__email", flat=True)
         cadmin_email_list = list(all_community_admins)
 
-        weekly_signups = data.get('weekly_sign_ups').filter(community__name=community_name).first()
-        community_weekly_signup = weekly_signups['signups'] if weekly_signups else 0
+        weekly_signups = (
+            data.get("weekly_sign_ups").filter(community__name=community_name).first()
+        )
+        community_weekly_signup = weekly_signups["signups"] if weekly_signups else 0
 
-        weekly_done_actions = data.get('weekly_done_actions').filter(action__community__name=community_name).first()
-        community_weekly_done_actions = weekly_done_actions['actions'] if weekly_done_actions else 0
+        weekly_done_actions = (
+            data.get("weekly_done_actions")
+            .filter(action__community__name=community_name)
+            .first()
+        )
+        community_weekly_done_actions = (
+            weekly_done_actions["actions"] if weekly_done_actions else 0
+        )
 
-        weekly_todo_actions = data.get('weekly_todo_actions').filter(action__community__name=community_name).first()
-        community_weekly_todo_actions = weekly_todo_actions['actions'] if weekly_todo_actions else 0
-        
+        weekly_todo_actions = (
+            data.get("weekly_todo_actions")
+            .filter(action__community__name=community_name)
+            .first()
+        )
+        community_weekly_todo_actions = (
+            weekly_todo_actions["actions"] if weekly_todo_actions else 0
+        )
 
-
-        temp_data ={
-            'community': community_name,
-            'signups': community_weekly_signup,
-            'actions_done': community_weekly_done_actions,
-            'actions_todo': community_weekly_todo_actions,
-            'start': str(one_week_ago.date()),
-            'end': str(today.date()),
-
+        temp_data = {
+            "community": community_name,
+            "signups": community_weekly_signup,
+            "actions_done": community_weekly_done_actions,
+            "actions_todo": community_weekly_todo_actions,
+            "start": str(one_week_ago.date()),
+            "end": str(today.date()),
         }
-        
 
-        send_nudge(None, None,cadmin_email_list, CADMIN_EMAIL_TEMPLATE_ID,temp_data)
+        send_nudge(None, None, cadmin_email_list, CADMIN_EMAIL_TEMPLATE_ID, temp_data)
     return "Success"
 
 
-
 def send_nudge(file, file_name, email_list, temp_id, t_model):
-    send_massenergize_email_with_attachments(temp_id, t_model, email_list, file, file_name)
+    send_massenergize_email_with_attachments(
+        temp_id, t_model, email_list, file, file_name
+    )
+
+
+def send_mou_email(email):
+    title = "MassEnergize Memorandum of Understanding(MOU)"
+    pass
+
+
+def update_records(**kwargs):
+    last = kwargs.get("last", None)
+    notices = kwargs.get("notices", [])
+    user = kwargs.get("user", None)
+    if not last:
+        record = PolicyAcceptanceRecords(user=user, notices=notices)
+        record.save()
+    elif last:
+        last.last_notified = notices
+        last.save()
+
+
+# Function to send MOU notifications to community admins
+def send_admin_mou_notification():
+    """
+    This function sends MOU (Memorandum of Understanding) notifications to all active community admins. It retrieves the last MOU record signed by each admin, checks if it's been over a year since they last signed, and sends an email notification if necessary. A timestamp of the latest notification is added to the policy record. If the admin has never been notified, then the function will record the current timestamp as the first notification. If there is no previous MOU record for the admin, the function assumes that they have never signed the MOU before and sends an MOU email to the admin.
+    """
+
+    # Get current time in UTC timezone
+    now = datetime.datetime.now(timezone.utc)
+
+    # Calculate one year and one month ago for comparison
+    a_year_ago = now - datetime.timedelta(days=365)
+    a_month_ago = now - datetime.timedelta(days=31)
+
+    # Filter all active community admins in the user profile
+    admins = UserProfile.objects.filter(is_deleted=False, is_community_admin=True)
+
+    for admin in admins:
+
+        try:
+            # Get last MOU record signed by the admin
+            last_record = admin.accepted_policies.filter(
+                type=PolicyConstants.mou()
+            ).latest("signed_at")
+
+            # Check if it's been more than a year since they last signed
+            more_than_a_year = last_record.signed_at <= a_year_ago
+
+            # If it's time to notify the admin again, then add a new notification timestamp to their policy record
+            if more_than_a_year:
+                notices = last_record.last_notified or []
+                last_date_of_notification = notices[len(notices) - 1]
+                more_than_a_month = last_date_of_notification <= a_month_ago
+
+                # Record the current notification timestamp
+                new_notification_time = datetime.datetime.now(timezone.utc)
+                notices.append(new_notification_time)
+
+                # Send MOU email to the admin and update their policy record with the new notification timestamp(s)
+                if (
+                    not last_date_of_notification
+                ):  # If for some reason notification date has never been recorded
+                    send_mou_email(admin.email)
+                    update_records(last=last_record, notices=notices)
+                else:  # They have been notified before
+                    more_than_a_month = last_date_of_notification <= a_month_ago
+                    if more_than_a_month:
+                        send_mou_email(admin.email)
+                        update_records(last=last_record, notices=notices)
+
+        except ObjectDoesNotExist:
+            # If no MOU record exists for the admin, this means the first time they need to sign the MOU
+            send_mou_email(admin.email)
+
+            # Record the current notification timestamp
+            new_notification_time = datetime.datetime.now(timezone.utc)
+            update_records(notices=[new_notification_time], user=admin)
