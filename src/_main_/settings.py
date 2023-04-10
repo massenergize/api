869d01d646b13a134393314a4e29768a04d68c00
tahiru <abdullai.tahiru@gmail.com>
2023-04-10 10:01:05 +0000

    django email  backend back

--- src/_main_/settings.py
+++ src/_main_/settings.py
@@ -265,7 +265,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-EMAIL_BACKEND = "anymail.backends.postmark.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True 
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587 
