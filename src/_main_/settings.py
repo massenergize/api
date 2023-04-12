079e0cae8b8355edbc6fdd35044a0551918f891a
Brad Hubbard-Nelson <bradhn@mindspring.com>
2023-04-12 16:30:55 -0400

    Settings problem
    RUN_CELERY_LOCALLY = False!

--- src/_main_/settings.py
+++ src/_main_/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # DJANGO_ENV can be passed in through the makefile, with "make start env=local"
 DJANGO_ENV = os.environ.get("DJANGO_ENV","remote")
 RUN_SERVER_LOCALLY = False
-RUN_CELERY_LOCALLY = True
+RUN_CELERY_LOCALLY = False
 
 if is_test_mode():
     RUN_CELERY_LOCALLY = True
