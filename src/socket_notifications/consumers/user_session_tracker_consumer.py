2315ca86b39dfb9b669fcedced0dc9409d5f752c
frimpongopoku <pongofrimi@Gmail.com>
2023-04-26 13:52:52 +0400

    con established, and sess expired now uses real data :fire:, and even easy scenario for test

--- src/socket_notifications/consumers/user_session_tracker_consumer.py
+++ src/socket_notifications/consumers/user_session_tracker_consumer.py
@@ -11,27 +11,29 @@ from api.utils.constants import WHEN_USER_AUTHENTICATED_SESSION_EXPIRES
 
 USER_SESSION_HAS_EXPIRED = "user_session_expired"
 UKNOWN_EXPIRY_TIME = "uknown_expiry_time"
+CONNECTION_ESTABLISHED = "connection_established"
 
 class UserSessionTrackerConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         await self.accept() 
-       
+        print("Well, we are connecting here ---- mtseeew ----")
         await self.send(text_data=json.dumps({
-            "type":"CONNECTION_ESTABLISHED", 
+            "type":CONNECTION_ESTABLISHED, 
             "message":"Its safe, send gift!"
-           
         }))
+
+        await self.track()
         
     async def send_auth_notification(self): 
         response = {"type":USER_SESSION_HAS_EXPIRED,"message":"User needs to sign in again..."}
         await self.send(json.dumps(response))
 
-    async def receive(self, text_data=None, bytes_data=None):
+    async def track(self):
         # return await super().receive(text_data, bytes_data)
         session = self.scope.get("session")
         expiration_in_session = session.get(WHEN_USER_AUTHENTICATED_SESSION_EXPIRES)
-    
+
         # If there is ever a situation where the expiration time is not sent through by the frontend 
         # Send a message back to send (There on frontend, admins can be forced to sign in again, if the value is really somehow not available)
         if not expiration_in_session: 
@@ -42,20 +44,19 @@ class UserSessionTrackerConsumer(AsyncWebsocketConsumer):
         expiration_as_date = datetime.fromtimestamp(in_seconds,tz=pytz.UTC)
 
         # --- FOR TESTING, UNCOMMENT THIS PART FOR A SHORTER WAIT
-        # expiration_as_date = current_date_and_time + timedelta(seconds=15)
+        expiration_as_date = datetime.now(tz=pytz.UTC) + timedelta(seconds=15)
 
      
 
         while True: 
             current_date_and_time = datetime.now(tz=pytz.UTC)
             if current_date_and_time > expiration_as_date: 
+                print("This shit happehned")
                 await self.send_auth_notification() 
                 break 
             await asyncio.sleep(5) 
             print("Still counting...") # REMOVE BEFORE PR (BPR)
     
-        
-           
 
        
 
