16f6d8d313485c058bee02f50bcb3d5b9240db54
frimpongopoku <pongofrimi@Gmail.com>
2023-05-31 09:20:56 +0400

    clean up

--- src/socket_notifications/consumers/user_session_tracker_consumer.py
+++ src/socket_notifications/consumers/user_session_tracker_consumer.py
@@ -9,6 +9,7 @@ import pytz
 
 from api.utils.constants import WHEN_USER_AUTHENTICATED_SESSION_EXPIRES
 
+USER_SESSION_RENEWED = "user_session_renewed"
 USER_SESSION_ALMOST_EXPIRED = "user_session_almost_expired"
 USER_SESSION_HAS_EXPIRED = "user_session_expired"
 CONNECTION_ESTABLISHED = "connection_established"
@@ -27,6 +28,7 @@ class UserSessionTrackerConsumer(AsyncWebsocketConsumer):
 
     async def get_expiration_as_date(self): 
         session = self.scope.get("session")
+        if not session: return None
         expiration_in_session = session.get(WHEN_USER_AUTHENTICATED_SESSION_EXPIRES)
         in_seconds = expiration_in_session / 1000
         expiration_as_date = datetime.fromtimestamp(in_seconds, tz=pytz.UTC)
@@ -36,41 +38,23 @@ class UserSessionTrackerConsumer(AsyncWebsocketConsumer):
         response = {"type": USER_SESSION_HAS_EXPIRED, "message": "User needs to sign in again..."}
         await self.send(json.dumps(response))
 
-    async def almost_expired(self):
-        response = {"type": USER_SESSION_ALMOST_EXPIRED, "message": "User session expires in 10 minutes..."}
-        await self.send(json.dumps(response))
-        await self.count_down_to_expiration()
-
-    
-    async def count_down_to_expiration(self): 
-        # ten_minutes_expiration = datetime.now(tz=pytz.UTC) + timedelta(minutes=10)
-        ten_minutes_expiration = await self.get_expiration_as_date()
-        # --- FOR TESTING, UNCOMMENT THIS ----
-        # ten_minutes_expiration = datetime.now(tz=pytz.UTC) + timedelta(minutes=1) # COMMENT OUT BEFORE PR (BPR)
-        self.expiry_task = asyncio.create_task(self.check_expiry(ten_minutes_expiration, self.send_auth_notification,60))
-
     async def track(self):
-        # session = self.scope.get("session")
-        # expiration_in_session = session.get(WHEN_USER_AUTHENTICATED_SESSION_EXPIRES)
-
-        # # If there is ever a situation where the expiration time is not sent through by the frontend 
-        # # Send a message back to send (There on frontend, admins can be forced to sign in again, if the value is really somehow not available)
-        # if not expiration_in_session:
-        #     response = {"type": USER_SESSION_HAS_EXPIRED, "message": "Did not receive session expiration time..."}
-        #     await self.send(json.dumps(response))
-        #     return
-
-        # in_seconds = expiration_in_session / 1000
-        # expiration_as_date = datetime.fromtimestamp(in_seconds, tz=pytz.UTC)
         expiration_as_date = await self.get_expiration_as_date()
-        expiration_as_date = expiration_as_date - timedelta(minutes=10)
-        self.expiry_task = asyncio.create_task(self.check_expiry(expiration_as_date, self.almost_expired))
+        # If there is ever a situation where the expiration time is not sent through by the frontend 
+        # Send a message back to send (There on frontend, admins can be forced to sign in again, if the value is really somehow not available)
+        if not expiration_as_date:
+            response = {"type": USER_SESSION_HAS_EXPIRED, "message": "Did not receive session expiration time..."}
+            await self.send(json.dumps(response))
+            return
 
-
-        # --- FOR TESTING, UNCOMMENT THIS PART FOR A SHORTER WAIT
+        # --- UNCOMMENT THIS IF YOU ARE LOCALLY TESTING 
+        # --- So that you send a disconnect message to the frontend in 25 seconds
         # expiration_as_date = datetime.now(tz=pytz.UTC) + timedelta(seconds=25)
-        # self.expiry_task = asyncio.create_task(self.check_expiry(expiration_as_date, self.almost_expired,5)) # COMMENT OUT BEFORE PR (BPR)
+        # self.expiry_task = asyncio.create_task(self.check_expiry(expiration_as_date, self.send_auth_notification,5))
 
+        # --- COMMENT THIS PART OUT IF YOU ARE LOCALLY TESTING
+        self.expiry_task = asyncio.create_task(self.check_expiry(expiration_as_date, self.send_auth_notification))
+       
        
     async def check_expiry(self, expiration_as_date, func, wait_time = WAIT_TIME):
         while True:
@@ -79,6 +63,12 @@ class UserSessionTrackerConsumer(AsyncWebsocketConsumer):
                 await func()
                 break
             await asyncio.sleep(wait_time) 
+
+    async def receive(self, text_data=None, bytes_data=None):
+        message = json.loads(text_data)
+        _type = message.get('type', None)
+        if _type == USER_SESSION_RENEWED: 
+            await self.track()
 
     async def disconnect(self, close_code):
         if hasattr(self, 'expiry_task'):
