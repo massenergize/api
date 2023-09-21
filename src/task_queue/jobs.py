051b2613fab119a01a1551f3d64ac040ac290682
tahiru <abdullai.tahiru@gmail.com>
2023-09-21 21:47:43 +0000

    added function as a task

--- src/task_queue/jobs.py
+++ src/task_queue/jobs.py
@@ -2,6 +2,7 @@
 from task_queue.events_nudge.cadmin_events_nudge import send_events_nudge
 from task_queue.events_nudge.user_event_nudge import prepare_user_events_nudge
 from task_queue.nudges.postmark_sender_signature import collect_and_create_signatures
+from task_queue.nudges.contents_spacing_correction import correct_contents_spacing
 from task_queue.views import super_admin_nudge, create_snapshots, send_admin_mou_notification
 
 """
@@ -19,4 +20,5 @@ FUNCTIONS = {
     "User Event Nudge": prepare_user_events_nudge,
     "Create Community Snapshots": create_snapshots,
     "Postmark Sender Signature": collect_and_create_signatures,
+    "Correct Contents Spacing": correct_contents_spacing
 }
--- src/task_queue/nudges/contents_spacing_correction.py
+++ src/task_queue/nudges/contents_spacing_correction.py
@@ -1,41 +1,44 @@
 from database.models import Action, Event, Testimonial
 
-# criteria_list = ["<p><br></p>", "<p>.</p>", "<p>&nbsp;</p>"]
-
 
 def correct_spacing_in_actions():
     actions = Action.objects.filter(is_deleted=False)
-    print(f"== Correcting spacing in {actions.count()}actions ==")
     for action in actions:
         about = action.about.replace("<p><br></p>", "").replace("<p>.</p>", "").replace("<p>&nbsp;</p>", "")
         deep_dive = action.deep_dive.replace("<p><br></p>", "").replace("<p>.</p>", "").replace("<p>&nbsp;</p>", "")
         action.about = about
         action.deep_dive = deep_dive
         action.save()
-    print("== Done Correcting spacing in actions ==")
+    return actions.count()
 
 
 
 def correct_spacing_in_events():
     events = Event.objects.filter(is_deleted=False)
-    print(f"== Correcting spacing in {events.count()}events ==")
     for event in events:
         description = event.description.replace("<p><br></p>", "").replace("<p>.</p>", "").replace("<p>&nbsp;</p>", "")
         event.description = description
         event.save()
-    print("== Done Correcting spacing in events==")
+    return events.count()
 
 
 def correct_spacing_in_testimonials():
     testimonials = Testimonial.objects.filter(is_deleted=False)
-    print(f"== Correcting spacing in {testimonials.count()}testimonials ==")
     for testimonial in testimonials:
         body = testimonial.body.replace("<p><br></p>", "").replace("<p>.</p>", "").replace("<p>&nbsp;</p>", "")
         testimonial.body = body
         testimonial.save()
-    print("== Done Correcting spacing in testimonials ==")
+    return testimonials.count()
 
 def correct_contents_spacing():
-    correct_spacing_in_actions()
-    correct_spacing_in_events()
-    correct_spacing_in_testimonials()
+    actions = correct_spacing_in_actions()
+    print(f"==Corrected {actions} Actions spacing==")
+    events = correct_spacing_in_events()
+    print(f"==Corrected {events} Actions spacing==")
+    testimonials = correct_spacing_in_testimonials()
+    print(f"==Corrected {testimonials} Actions spacing==")
+    return True
+    
+
+
+
