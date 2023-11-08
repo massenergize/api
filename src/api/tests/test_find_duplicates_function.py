22f88912c438b9d82ec54c510eeb955be2fe0332
frimpongopoku <pongofrimi@gmail.com>
2023-11-08 12:17:33 +0000

    finalised find dupes test.

--- src/api/tests/test_find_duplicates_function.py
+++ src/api/tests/test_find_duplicates_function.py
@@ -2,14 +2,21 @@ from django.test import TestCase
 from _main_.utils.utils import Console
 from api.store.common import find_duplicate_items
 from api.tests.common import createImage, makeCommunity, makeMedia, makeUserUpload
-
 from database.models import Media
 
 
 # python manage.py test api.tests.test_find_duplicates_function.FindDuplicateItemsTest
 class FindDuplicateItemsTest(TestCase):
     def setUp(self):
+        """
+        The function should only return image duplicates that are related to a specified list of communities!
+        Meaning if one image has been duplicated about 4 times, but 2 of the duplicates belong to Wayland,
+        and 2 belong to Framingham, when we look for duplicates in Wayland, it should only return that there
+        are 2 duplicates of an image in Wayland.
+        """
         # Create communities
+        Console.header("Testing 'find_duplicates' function")
+        print("Creating communities...")
         community1, community2, community3 = [
             makeCommunity(subdomain="first"),
             makeCommunity(subdomain="second"),
@@ -18,8 +25,8 @@ class FindDuplicateItemsTest(TestCase):
         self.com1 = community1
         self.com2 = community2
         self.com3 = community3
-
         # Create some sample Media objects with duplicate hashes
+        print("Generating media and linking to communities...")
         self.media1 = makeMedia(name="Biden")
         self.media2 = makeMedia(name="Biden")
         self.media3 = makeMedia(name="Biden")
@@ -30,8 +37,8 @@ class FindDuplicateItemsTest(TestCase):
         self.media6 = makeMedia(name="Brad", image=createImage(link))
         self.mediapro = makeMedia(name="Media Pro")
 
-        # upload1, upload2, upload3, upload4, upload5, upload6, pro 
-        
+        # upload1, upload2, upload3, upload4, upload5, upload6, pro
+
         uploads = [
             makeUserUpload(media=self.media1, communities=[community1]),
             makeUserUpload(media=self.media2, communities=[community1]),
@@ -41,23 +48,30 @@ class FindDuplicateItemsTest(TestCase):
             makeUserUpload(media=self.media6, communities=[community2]),
             makeUserUpload(media=self.mediapro, communities=[community3]),
         ]
-     
 
     def test_find_duplicate_items(self):
-        # Call the find_duplicate_items function
-        set1 = find_duplicate_items(
-            False, community_ids=[self.com2.id]
-        )  # Should find me dupes here
-        # set2 = find_duplicate_items(False,community_ids=[self.com2.id])  # Should not find any dupes here
-        # Console.log("SET 1",set1)
-        # Console.log("SET 2",set1)
-        Console.underline("SET 1")
-        print("SET 1", set1)
+        dupes_in_com1 = find_duplicate_items(False, community_ids=[self.com1.id])
+        dupes_in_com2 = find_duplicate_items(False, community_ids=[self.com2.id])
+        dupes_in_multiple_coms = find_duplicate_items(
+            False, community_ids=[self.com1.id, self.com3.id]
+        )
 
-        # print("SET 2", set2)
+        dupes_in_com1 = list(dupes_in_com1.values())[0]
+        dupes_in_com2 = list(dupes_in_com2.values())[0]
+        dupes_in_multiple_coms = list(dupes_in_multiple_coms.values())[0]
 
-    # def tearDown(self):
-    #     # Clean up by deleting the created Media objects
-    #     self.media1.delete()
-    #     self.media2.delete()
-    #     self.media3.delete()
+        dupes_in_com1 = [m.id for m in dupes_in_com1]
+        dupes_in_com2 = [m.id for m in dupes_in_com2]
+        dupes_in_multiple_coms = [m.id for m in dupes_in_multiple_coms]
+
+        self.assertEquals(len(dupes_in_com1), 2)
+        self.assertIn(self.media1.id, dupes_in_com1)
+        self.assertIn(self.media2.id, dupes_in_com1)
+        self.assertEquals(len(dupes_in_com2), 2)
+        self.assertIn(self.media5.id, dupes_in_com2)
+        self.assertIn(self.media6.id, dupes_in_com2)
+        self.assertEquals(len(dupes_in_multiple_coms), 3)
+        self.assertIn(self.media1.id, dupes_in_multiple_coms)
+        self.assertIn(self.media2.id, dupes_in_multiple_coms)
+        self.assertIn(self.mediapro.id, dupes_in_multiple_coms)
+        print("Identifying duplicates in specified communities works!")
