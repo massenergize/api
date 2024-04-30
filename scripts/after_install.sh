0fd3d270cb1f1cbb9bebf917acd47c6e97b8552e
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2024-04-29 19:05:57 -0700

    load_dotenv

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,4 +5,4 @@ cd /webapps/massenergize_api
 sudo docker-compose down
 
 # Build the new Docker images
-# sudo docker-compose build
+sudo docker-compose build
--- src/_main_/utils/stage/secrets.py
+++ src/_main_/utils/stage/secrets.py
@@ -26,6 +26,7 @@ def get_secret(stage):
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
         )
+        load_dotenv()
     except ClientError as e:
         print("Could not fetch credentials")
         return load_env(stage)
