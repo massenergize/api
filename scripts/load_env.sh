6be4e51b01faa7558687c73b709d698b975a79c3
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2024-04-29 19:43:38 -0700

    ENV_FILE

--- scripts/load_env.sh
+++ scripts/load_env.sh
@@ -2,25 +2,31 @@
 
 cd /webapps/massenergize_api
 
-# Create the .env file if it doesn't exist
-sudo touch .env
 
-# Loop through all environment variables
-for var in $(env); do
-  # Extract the variable name and value
-  name="${var%=*}"
-  value="${var#*=}"
+# File to store environment variables
+ENV_FILE=".env"
 
-  # Skip variables starting with '_' (usually internal variables)
-  if [[ $name == '_' ]]; then
-    continue
-  fi
+# Remove existing .env file if it exists
+if [ -f "$ENV_FILE" ]; then
+    rm "$ENV_FILE"
+fi
 
-  # Skip variables starting with %
-  if [[ $name == ^% ]]; then
-    continue
-  fi
+sudo touch $ENV_FILE
 
-  # Append the variable to the .env file in the format "NAME=VALUE"
-  sudo echo "$name=$value" >> .env
-done
+# Check if /etc/profile exists
+if [ -f "/etc/profile" ]; then
+    # Read each line in /etc/profile
+    while IFS= read -r line
+    do
+        # Check if the line starts with "export"
+        if [[ "$line" == export\ * ]]; then
+            # Extract the variable name and value
+            var_name=$(echo "$line" | cut -d' ' -f2 | cut -d'=' -f1)
+            var_value=$(echo "$line" | cut -d'=' -f2-)
+            # Append the variable and value to .env file
+            sudo echo "$var_name=$var_value" >> "$ENV_FILE"
+        fi
+    done < "/etc/profile"
+fi
+
+echo "Environment variables from /etc/profile copied to $ENV_FILE"
