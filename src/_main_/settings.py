79bd355a1c341eb76a2eedfa6a6d7f55f271b14d
Abdullai Tahiru <42780120+abdullai-t@users.noreply.github.com>
2024-05-08 10:40:54 +0000

    reverted is_local

--- src/_main_/settings.py
+++ src/_main_/settings.py
@@ -32,7 +32,7 @@ DJANGO_ENV = os.environ.get("DJANGO_ENV","remote")
 # Database selection, development DB unless one of these chosen
 IS_PROD = False
 IS_CANARY = False
-IS_LOCAL = True
+IS_LOCAL = False
 
 RUN_SERVER_LOCALLY = IS_LOCAL
 RUN_CELERY_LOCALLY = IS_LOCAL
