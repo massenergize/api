df58e8e4ea83dc44983b6a9c527ffab7381c2c99
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-05-13 09:15:51 +0000

    Replace Slack notifications with logging in task queue
    Refactored the code for managing tasks in the task queue. Slack notifications were replaced with logging, which improves the efficiency of error tracking and troubleshooting. Function 'send_slack_notification' was removed and replaced by 'log_status' function that logs task status.

--- src/task_queue/tasks.py
+++ src/task_queue/tasks.py
@@ -1,57 +1,59 @@
 import datetime
+import logging
+
 from celery import shared_task
 from django.db import transaction
-from .models import Task
+
+from _main_.settings import IS_PROD
 from task_queue.helpers import is_time_to_run
 from .jobs import FUNCTIONS
-from api.services.utils import send_slack_message
-from _main_.settings import SLACK_CELERY_TASKS_WEBHOOK_URL, IS_PROD, IS_LOCAL, IS_CANARY
+from .models import Task
 
-ENV = "LOCAL" if IS_LOCAL else "CANARY" if IS_CANARY else "PRODUCTION" if IS_PROD else "DEVELOPMENT"
+# Initialize logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+SUCCESS_LOG_KEY = "CELERY_TASK_SUCCESS"
+FAILURE_LOG_KEY = "CELERY_TASK_FAILURE"
 
 
-def send_slack_notification(status, task_name, message):
-    today = datetime.date.today()
-    try:
-        send_slack_message(SLACK_CELERY_TASKS_WEBHOOK_URL, {
-            "task": task_name,
-            "status": "✅" if status == "SUCCEEDED" else "❌",
-            "date": str(today),
-            "message": message,
-            "environment": ENV
-        })
-    except Exception as e:
-        print(f"Failed to send slack notification: {e}")
+def log_status(status, task_name, message):
+	# if not IS_PROD:
+	# 	return
+	if status == "SUCCEEDED":
+		logger.info(f"Task: {task_name}, Status: ✅, Info: {message}, key:{SUCCESS_LOG_KEY}")
+	else:
+		logger.error(f"Task: {task_name}, Status: ❌, Info: {message}, key:{FAILURE_LOG_KEY}")
 
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={'max_retries': 1, 'countdown': 5})
 def run_some_task(self, task_id):
-    try:
-        today = datetime.date.today()
-        should_run = False
-        task = None
-        with transaction.atomic():
-            task = Task.objects.select_for_update().get(id=task_id)  # locks task instance until transaction is committed
-            if is_time_to_run(task):
-                task.last_run = today
-                task.save()
-                should_run = True
-
-        if task and should_run:
-            func = FUNCTIONS.get(task.job_name)
-            if func:
-                task.status = "SCHEDULED"
-                task.save()
-                try:
-                    res = func(task)
-                    task.status = "SUCCEEDED" if res else "FAILED"
-                    send_slack_notification(task.status, task.job_name, "Task ran successfully" if res else "Task failed")
-                except Exception as e:
-                    task.status = "FAILED"
-                    send_slack_notification(task.status, task.job_name, today, str(e))
-            else:
-                task.status = "FAILED"
-                send_slack_notification(task.status, task.job_name,  "No function found for job_name")
-            task.save()
-    except Exception as e:
-        send_slack_notification("FAILED", "unknown", str(e))
+	try:
+		today = datetime.date.today()
+		should_run = False
+		task = None
+		with transaction.atomic():
+			task = Task.objects.select_for_update().get(
+				id=task_id)  # locks task instance until transaction is committed
+			if is_time_to_run(task):
+				task.last_run = today
+				task.save()
+				should_run = True
+		
+		if task and should_run:
+			func = FUNCTIONS.get(task.job_name)
+			if func:
+				task.status = "SCHEDULED"
+				task.save()
+				try:
+					res = func(task)
+					task.status = "SUCCEEDED" if res else "FAILED"
+					log_status(task.status, task.job_name, "Task ran successfully" if res else "Task failed")
+				except Exception as e:
+					task.status = "FAILED"
+					log_status(task.status, task.job_name, today, str(e))
+			else:
+				task.status = "FAILED"
+				log_status(task.status, task.job_name, "No function found for job_name")
+			task.save()
+	except Exception as e:
+		log_status("FAILED", "unknown", str(e))
