7bbcbde6a83fa4ef0cd55937993e19fb21ef7f0f
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-05-21 10:37:51 +0000

    Refactor webhook processing and message retrieval logic
    Refactor the extract_msg_id function in the webhook service to improve parsing and error handling. Modify the process_inbound_webhook method to use the refactored message extraction method and expand on conditions for successful message processing. Update the get_message_info method in message store to account for inbound messages.

--- src/api/services/webhook.py
+++ src/api/services/webhook.py
@@ -1,3 +1,5 @@
+import logging
+
 from sentry_sdk import capture_message
 from _main_.utils.massenergize_errors import MassEnergizeAPIError
 from _main_.utils.context import Context
@@ -34,18 +36,25 @@ def extract_email_content(text):
 
 
 def extract_msg_id(text):
-    link = text.strip().split("and respond to message")
-    link = link[1].strip()
-    url_match = re.search(r'<(.*?)>', link)
-    url = url_match.group(1)
-    if url:
-      parsed_url = urllib.parse.urlparse(url)
-      path = parsed_url.path
-      path = urllib.parse.unquote(path)
-      id = path.split("/")
-      return id[5]
-    return None
-       
+    try:
+        text_link = text.strip().split("and respond to message")
+        text_link = text_link[0].strip()
+        url_pattern = r"https://click\.pstmrk\.it\S+"
+        url = re.findall(url_pattern, text_link)
+ 
+        if not len(url):
+            return None
+        
+        parsed_url = urllib.parse.urlparse(url[0])
+        path = parsed_url.path
+        path = urllib.parse.unquote(path)
+        id = path.split("/")
+        return id[5]
+    
+    except Exception as e:
+        logging.error(str(e))
+        return None
+    
 
    
 
@@ -73,35 +82,40 @@ class WebhookService:
 
     return {"success":True}, None
   
-
-
-  
   def process_inbound_webhook(self, context: Context, args) -> Tuple[dict, MassEnergizeAPIError]:
     try:
         reply = args.get("StrippedTextReply")
         text_body = args.get("TextBody")
         from_email = args.get("From")
 
-        splitted_body = text_body.strip().split("Here is a copy of the message:")
-        if len(splitted_body) < 2: # will probably be a postmark test
+        split_body = text_body.strip().split("Here is a copy of the message:")
+        
+        if len(split_body) < 2: # will probably be a postmark test
           return {"success":False},None
-        user_msg_content = splitted_body[1].strip().split("If possible, please reply through the admin portal rather than")[0].strip()
+        
+        user_msg_content = split_body[1].strip().split("If possible, please reply through the admin portal rather than")[0].strip()
         subject,email = extract_email_content(user_msg_content)
 
-        db_msg_id = extract_msg_id(splitted_body[0])
-        if db_msg_id and email:
-          res, err = self.message_service.reply_from_community_admin(context, {
+        db_msg_id = extract_msg_id(split_body[0])
+
+        if not db_msg_id and not email:
+            return {"success":False}, None
+        
+        res, err = self.message_service.reply_from_community_admin(context, {
             "title": f"Re: {subject}",
             "body": reply,
-            "to": email, 
+            "to": email,
             "message_id": db_msg_id,
-            "from_email": from_email
-          })
+            "from_email": from_email,
+            "is_inbound": True,
+            "email": email
+            })
 
-          if err:
-            return None, str(err)
-          
-          return {"success":True}, None
+        if err:
+          return None, str(err)
+    
+        return {"success":True}, None
+        
     except Exception as e:
       capture_message(str(e), level="error")
       return None, MassEnergizeAPIError(e)
