ab54ba2e92926c208268b64bd342ba4cf4c86d84
Kobina_Koomson <georgeranch31@gmail.com>
2024-05-24 14:07:29 +0000

    feat: add cached function decorator
    This commit adds a new decorator 'cached_function'. This decorator takes an input function, and checks for existing cache values based on the function name, subdomain, and locale. If no cache exists or if a 'force_refresh' parameter is set to true, it runs the input function and caches its result.

--- src/api/decorators.py
+++ src/api/decorators.py
@@ -1,17 +1,19 @@
 """
-This contains custom decorators that can be used to check that specific 
+This contains custom decorators that can be used to check that specific
 conditions have been satisfied
 """
 from django.core.exceptions import PermissionDenied
 from _main_.utils.context import Context
 from functools import wraps
-from django.views.decorators.http import require_http_methods
+from django.core.cache import cache
 
+from _main_.utils.massenergize_errors import CustomMassenergizeError
 
 def login_required(function):
   """
   This decorator enforces that a user is logged in before a view can run
   """
+
   @wraps(function)
   def wrap(handler, request, *args, **kwargs):
     context: Context = request.context
@@ -24,11 +26,11 @@ def login_required(function):
   wrap.__name__ = function.__name__
   return wrap
 
-
 def admins_only(function):
   """
   This decorator enforces that the user is an admin before the view can run
   """
+
   @wraps(function)
   def wrap(handler, request, *args, **kwargs):
     context: Context = request.context
@@ -36,6 +38,7 @@ def admins_only(function):
       return function(handler, request, *args, **kwargs)
     else:
       raise PermissionDenied
+
   wrap.__doc__ = function.__doc__
   wrap.__name__ = function.__name__
   return wrap
@@ -44,6 +47,7 @@ def community_admins_only(function):
   """
   This decorator enforces that the user is a community admin before the view can run
   """
+
   @wraps(function)
   def wrap(handler, request, *args, **kwargs):
     context: Context = request.context
@@ -51,6 +55,7 @@ def community_admins_only(function):
       return function(handler, request, *args, **kwargs)
     else:
       raise PermissionDenied
+
   wrap.__doc__ = function.__doc__
   wrap.__name__ = function.__name__
   return wrap
@@ -59,6 +64,7 @@ def super_admins_only(function):
   """
   This decorator enforces that a user is a super admin before the view can run
   """
+
   @wraps(function)
   def wrap(handler, request, *args, **kwargs):
     context: Context = request.context
@@ -70,3 +76,29 @@ def super_admins_only(function):
   wrap.__doc__ = function.__doc__
   wrap.__name__ = function.__name__
   return wrap
+
+def cached_request(func):
+  @wraps(func)
+  def wrapper(handler, request, *args, **kwargs):
+    subdomain = request.context.args.get('subdomain', None)
+    locale = request.context.args.get('locale', "en")
+
+    if not subdomain:
+      return CustomMassenergizeError("You are Missing a Required Input: subdomain")
+
+    force_refresh = request.context.args.get('force_refresh', False)
+
+    if force_refresh:
+      cache.delete(f"{func.__module__}.{func.__name__}.{subdomain}.{locale}")
+
+    cache_key = f"{func.__module__}.{func.__name__}.{subdomain}.{locale}"
+    cached_data = cache.get(cache_key)
+
+    if cached_data and not force_refresh:
+      return cached_data
+    else:
+      result = func(handler, request, *args, **kwargs)
+      cache.set(cache_key, result,)
+      return result
+
+  return wrapper
