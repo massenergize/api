c7354a6f17e241174b72fd8c40729082100973c2
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2024-05-27 00:12:14 -0400

    set IFS

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -5,16 +5,19 @@
 # Function to check and kill process on a given port
 check_and_kill_process() {
     local port="$1"
-    local pid=$(lsof -t -i:"$port")
+    local pids=$(lsof -t -i:"$port")
 
-    echo "PID: $pid, PORT: $port."
-
-    if [ -z "$pid" ]; then
+    if [ -z "$pids" ]; then
         echo "No process is running on port $port."
     else
-        # Kill the process
-        kill -9 "$pid"
-        echo "Killed process $pid running on port $port."
+        # Set IFS to newline to handle newline-separated values
+        IFS=$'\n'
+        for pid in $pids; do
+            kill -9 "$pid"
+            echo "Killed process $pid running on port $port."
+        done
+        # Reset IFS to default value
+        unset IFS
     fi
 }
 
