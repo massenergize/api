6d1a7b2f728579009554333c056d9ae5df9701c3
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2024-05-26 23:31:04 -0400

    kill any application on port 8000 first

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,6 +2,19 @@
 
 cd /webapps/massenergize/api
 
+# Find the process ID (PID) using lsof to check for any process listening on port 8000
+PID=$(lsof -t -i:8000)
+
+# Check if a PID was found
+if [ -z "$PID" ]; then
+    echo "No process is running on port 8000."
+else
+    # Kill the process
+    kill -9 $PID
+    echo "Killed process $PID running on port 8000."
+fi
+
+
 # Start the new Docker containers
 sudo docker-compose up -d
 
