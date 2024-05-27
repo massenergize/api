3b28521a30892b1df28be39af412a765ee7b068b
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2024-05-27 00:54:03 -0400

    refactor

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,10 +4,12 @@ cd /webapps/massenergize/api
 
 sudo docker-compose down
 sudo docker-compose down --remove-orphans
+sudo docker stop $(docker ps -q) && sudo docker rm $(docker ps -aq)
+sudo service docker restart
 sudo docker image prune -f
 sudo docker builder prune -f
 
-# sudo service docker restart
+
 
 # Build the new Docker images
 sudo docker-compose build
