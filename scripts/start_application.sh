f8b46727ff5768205325fb0d0f55befa8c4514c9
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2024-05-29 05:16:33 -0400

    update startup script

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,31 +2,6 @@
 
 cd /webapps/massenergize/api
 
-# Function to check and kill process on a given port
-# check_and_kill_process() {
-#     local port="$1"
-#     local pids=$(lsof -t -i:"$port")
-
-#     if [ -z "$pids" ]; then
-#         echo "No process is running on port $port."
-#     else
-#         # Set IFS to newline to handle newline-separated values
-#         IFS=$'\n'
-#         for pid in $pids; do
-#             kill -9 "$pid"
-#             echo "Killed process $pid running on port $port."
-#         done
-#         # Reset IFS to default value
-#         unset IFS
-#     fi
-# }
-
-# # Check and kill process on port 8000
-# check_and_kill_process 8000
-
-# # Check and kill process on port 80
-# check_and_kill_process 80
-
 # Start the new Docker containers
 sudo docker-compose up -d
 
