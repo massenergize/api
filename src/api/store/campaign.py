9603c16f119d9edd7c9e9ec5b01cbd3d1649121f
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-05-30 14:07:05 +0000

    Refactor campaign technology event handling
    This commit involves refactoring of the implementation of add_campaign_technology_event method in the campaign.py file. The multiple campaign ids handling has been removed and replaced with handling for a single campaign id. This promises for a more direct, and less error-prone system.

--- src/api/store/campaign.py
+++ src/api/store/campaign.py
@@ -808,17 +808,16 @@ class CampaignStore:
     def add_campaign_technology_event(self, context: Context, args):
         try:
             campaign_technology_id = args.pop("campaign_technology_id", None)
-            campaign_technology_ids = args.pop("campaign_technology_ids", None)
             event_ids = args.pop("event_ids", None)
 
             created_list = []
 
-            if not campaign_technology_ids:
-                return None, CustomMassenergizeError("campaign_technology_ids is required!")
+            if not campaign_technology_id:
+                return None, CustomMassenergizeError("campaign_technology_id is required!")
             
-            campaign_techs = CampaignTechnology.objects.filter(id__in=campaign_technology_ids)
+            campaign_tech = CampaignTechnology.objects.filter(id=campaign_technology_id).first()
             
-            if not campaign_techs:
+            if not campaign_tech:
                 return None, CustomMassenergizeError("campaignTechnology with id not found!")
 
             if not event_ids:
