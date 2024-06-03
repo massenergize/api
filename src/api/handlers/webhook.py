59816ad2dd8938f6ff8c36cff06a3301f247cce3
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-06-03 08:24:05 +0000

    Add JSON Decode Error handling in webhook handlers
    Added a try-except block to handle JSON Decode Error during argument extraction from the request body in the "bounce_email_webhook" and "process_inbound_webhook" methods of WebhookHandler. This improvement will log the error and provide a better response in case of invalid JSON input.

--- src/api/handlers/webhook.py
+++ src/api/handlers/webhook.py
@@ -8,45 +8,56 @@ from _main_.utils.massenergize_response import MassenergizeResponse
 from _main_.utils.context import Context
 from api.services.webhook import WebhookService
 
+
 class WebhookHandler(RouteHandler):
-
-  def __init__(self):
-    super().__init__()
-    self.service = WebhookService()
-    self.registerRoutes()
-
-  def registerRoutes(self):
-    self.add("/email.bounce.webhook", self.bounce_email_webhook)
-    self.add("/webhooks.inbound.get", self.process_inbound_webhook)
-
-
-  def bounce_email_webhook(self, request):
-    try:
-      context: Context = request.context
-      args: dict = json.loads(request.body)
-      
-      res, err = self.service.bounce_email_webhook(context, args)
-      
-      if err:
-        return err
-      return MassenergizeResponse(data=res)
-    
-    except Exception as e:
-      logging.error(f"BOUNCE EMAIL WEBHOOK ERROR: {str(e)}")
-      return MassenergizeResponse(error=str(e))
-
-
-  def process_inbound_webhook(self, request):
-      try:
-          context: Context = request.context
-          args: dict = json.loads(request.body)
-          
-          res, err = self.service.process_inbound_webhook(context, args)
-          
-          if err:
-              return err
-          return MassenergizeResponse(data=res)
-      
-      except Exception as e:
-        logging.error(f"INBOUND WEBHOOK ERROR: {str(e)}")
-        return MassenergizeResponse(error=str(e))
+	
+	def __init__(self):
+		super().__init__()
+		self.service = WebhookService()
+		self.registerRoutes()
+	
+	def registerRoutes(self):
+		self.add("/email.bounce.webhook", self.bounce_email_webhook)
+		self.add("/webhooks.inbound.get", self.process_inbound_webhook)
+	
+	def bounce_email_webhook(self, request):
+		try:
+			context: Context = request.context
+			
+			try:
+				args = json.loads(request.body)
+			
+			except json.JSONDecodeError as e:
+				logging.error(f'JSON_FORMAT_ERROR:{str(e)}')
+				return MassenergizeResponse(error=str(e))
+			
+			res, err = self.service.bounce_email_webhook(context, args)
+			
+			if err:
+				return err
+			return MassenergizeResponse(data=res)
+		
+		except Exception as e:
+			logging.error(f"BOUNCE EMAIL WEBHOOK ERROR: {str(e)}")
+			return MassenergizeResponse(error=str(e))
+	
+	def process_inbound_webhook(self, request):
+		try:
+			context: Context = request.context
+			
+			try:
+				args = json.loads(request.body)
+			
+			except json.JSONDecodeError as e:
+				logging.error(f'JSON_FORMAT_ERROR:{str(e)}')
+				return MassenergizeResponse(error=str(e))
+			
+			res, err = self.service.process_inbound_webhook(context, args)
+			
+			if err:
+				return err
+			return MassenergizeResponse(data=res)
+		
+		except Exception as e:
+			logging.error(f"INBOUND WEBHOOK ERROR: {str(e)}")
+			return MassenergizeResponse(error=str(e))
