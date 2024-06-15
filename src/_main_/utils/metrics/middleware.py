41448276c4bcc8b0e11cb7f98b090dcbfa961336
Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2024-06-14 21:15:20 -0700

    we are getting charged too much for storing metrics on on api endpoints.  I think for now we can just log the latency and build a dashboard on that.  We can track latency rather for the handler functions which does everything anyways.

--- src/_main_/utils/metrics/middleware.py
+++ src/_main_/utils/metrics/middleware.py
@@ -18,55 +18,14 @@ class MetricsMiddleware(MiddlewareMixin):
         request.start_time = time.time()
 
     def process_response(self, request, response):
-        self.send_cw_metrics(request)
+        self.log_the_time_taken_to_complete_request(request)
         return response
 
     @run_in_background
-    def send_cw_metrics(self, request):
+    def log_the_time_taken_to_complete_request(self, request):
         if not hasattr(request, 'start_time'):
             return
-        
         latency = (time.time() - request.start_time) * 1000 # convert to milliseconds
-            
-        try:
-            if not EnvConfig.can_send_logs_to_cloudwatch():
-                logger.info(f"Path: {request.path} Latency: {latency}ms")
-                return 
+        logger.info(f"Path: {request.path} Latency(ms): {latency}")
+        logger.info()
 
-            self.cloudwatch.put_metric_data(
-                        Namespace=f'{EnvConfig.name.title()}MassenergizeApiService',
-                        MetricData=[
-                        {
-                            'MetricName': 'Latency',
-                            'Dimensions': [
-                                {
-                                    'Name': 'Endpoint',
-                                    'Value': request.path
-                                },
-                                {
-                                    'Name': 'Stage',
-                                    'Value': EnvConfig.name
-                                }
-                            ],
-                            'Unit': 'Milliseconds',
-                            'Value': latency                            
-                        },
-                        {
-                            'MetricName': 'Count',
-                            'Dimensions': [
-                                {
-                                    'Name': 'Endpoint',
-                                    'Value': request.path
-                                },
-                                {
-                                    'Name': 'Stage',
-                                    'Value': EnvConfig.name
-                                }
-                            ],
-                            'Unit': 'Count',
-                            'Value': 1
-                        },
-                    ]
-                    )
-        except Exception:
-            logging.error("AWS credentials not configured properly.")
