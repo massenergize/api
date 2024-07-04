05d08a63b3c05d346543506018d94b53f6032eec
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-07-04 13:09:42 +0000

    feat: Added an endpoint for getting all languages
    Integrated a new translation service to return all available languages. This involves the creation of `translation.py` files in handlers, services and store directories. Also updated `urls.py` to register this new TranslationsHandler.

---
+++ src/api/handlers/translation.py
@@ -0,0 +1,29 @@
+from _main_.utils.massenergize_response import MassenergizeResponse
+from _main_.utils.route_handler import RouteHandler
+from api.services.translation import TranslationsService
+from _main_.utils.context import Context
+
+
+class TranslationsHandler(RouteHandler):
+	def __init__(self):
+		super().__init__()
+		
+		self.service = TranslationsService()
+		self.registerRoutes()
+	
+	def registerRoutes(self):
+		self.add("/translations.languages.get", self.get_all_languages)
+	
+	def get_all_languages(self, request):
+		context: Context = request.context
+		args: dict = context.args
+		
+		args, err = self.validator.verify(args)
+		
+		if err:
+			return err
+		
+		all_languages, err = self.service.get_all_languages(context, args)
+		if err:
+			return err
+		return MassenergizeResponse(data=all_languages)
---
+++ src/api/services/translation.py
@@ -0,0 +1,22 @@
+from _main_.utils.common import serialize
+from api.store.translation import TranslationsStore
+
+
+class TranslationsService:
+	"""
+	Service Layer for all the translations
+	"""
+	
+	def __init__(self):
+		self.store = TranslationsStore()
+	
+	def get_all_languages(self, context, args) -> (dict, Exception):
+		"""
+		Get all the languages
+		"""
+		all_languages, err = self.store.get_all_languages(context, args)
+		
+		if err:
+			return None, err
+		
+		return all_languages, None
