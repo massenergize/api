9faf5929cba7ea5b48aed9f38c5433d870e6278f
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-07-17 12:13:10 +0000

    test: Replace `tz_aware_utc_now` with `parse_datetime_to_aware` and `timezone.now` in test files
    Updated the test files (`test_message.py`, `test_feature_flags.py`, `test_events.py`, `test_communities.py`) to remove the usage of `tz_aware_utc_now` function. Replaced it with two appropriate functions (`parse_datetime_to_aware` and `timezone.now`) to ensure accurate generation and manipulation of date and time in tests.

--- src/api/tests/integration/test_feature_flags.py
+++ src/api/tests/integration/test_feature_flags.py
@@ -4,7 +4,9 @@ This is the test file for feature flags
 from datetime import datetime, timedelta
 import time
 from django.test import TestCase, Client
-from _main_.utils.common import serialize, tz_aware_utc_now
+from django.utils import timezone
+
+from _main_.utils.common import serialize
 from _main_.utils.feature_flags.FeatureFlagConstants import FeatureFlagConstants
 from _main_.utils.utils import Console
 from database.models import FeatureFlag, Community, CommunityAdminGroup
@@ -162,7 +164,7 @@ class FeatureFlagHandlerTest(TestCase):
             audience=FeatureFlagConstants.for_specific_audience(),
         )
         all_community_nudge_flag = makeFlag(name="all-community-nudge")
-        today = tz_aware_utc_now()
+        today = timezone.now()
         last_week = today - timedelta(days=7)
 
         expired_flag = makeFlag(name="expired-flag",expires_on=last_week)
--- src/api/tests/integration/test_message.py
+++ src/api/tests/integration/test_message.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from _main_.utils.common import tz_aware_utc_now
+from _main_.utils.common import parse_datetime_to_aware
 from database.models import Community, Message
 from django.test import TestCase, Client
 from unittest.mock import patch
@@ -10,7 +10,7 @@ from _main_.utils.utils import Console
 
 
 def create_schedule(id=None):
-    in_5_days = tz_aware_utc_now() + timedelta(days=10 if id else 5)
+    in_5_days = parse_datetime_to_aware() + timedelta(days=10 if id else 5)
     schedule = in_5_days.strftime("%a, %d %b %Y %H:%M:%S GMT")
 
     return schedule
