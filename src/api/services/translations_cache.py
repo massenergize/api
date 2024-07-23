a2dadbda1f58bcefbc96927f34a5ef42da534d5e
Kobina_Koomson <georgeranch31@gmail.com>
2024-07-23 15:43:40 +0000

    refactor: Update type hints and improve readability in translations_cache.py
    This commit updates the type hints in the functions of the translations_cache.py file for clarity. The create_translation, get_translation, and get_target_languages methods were also refactored to improve readability. Minor adjustments were made to the translate_all_models method, especially in error logging and exception handling.

--- src/api/services/translations_cache.py
+++ src/api/services/translations_cache.py
@@ -11,7 +11,7 @@ from _main_.utils.utils import to_third_party_lang_code, run_in_background
 from api.store.supported_language import SupportedLanguageStore
 from api.store.translations_cache import TranslationsCacheStore
 from api.services.text_hash import TextHashService
-from typing import Tuple, Set
+from typing import Tuple, Set, Union
 
 
 DEFAULT_SOURCE_LANGUAGE_CODE = "en-US"
@@ -24,9 +24,10 @@ class TranslationsCacheService:
         self.translator = Translator()
         self.textHashService = TextHashService()
 
-    def create_translation (self, context: Context, args: dict) -> Tuple[ TranslationsCache or None, None ]:
+    def create_translation (self, args: dict) -> Tuple[ Union[TranslationsCache, None], None ]:
         """
-        This function creates a translation in the database
+        This is a wrapper function for the TranslationsCacheStore.create_translation method.
+         It passes the context and args to the store's create_translation method, which creates a new translation in the TranslationsCache table.
 
         Args:
         - context: Context
@@ -39,16 +40,21 @@ class TranslationsCacheService:
         Returns:
         - translation: TranslationsCache
         """
-        translation, err = self.store.create_translation(context, args)
+        translation, err = self.store.create_translation(args)
         return translation if translation else None, err
 
     def get_translation (self, context: Context, args: dict) -> Tuple[ TranslationsCache or None, None ]:
+        """
+        This is a wrapper function for the TranslationsCacheStore.get_translation method.
+         It passes the context and args to the store's get_translation method, which retrieves a translation from the TranslationsCache table.
+        """
         translation, err = self.store.get_translation(context, args)
         return translation if translation else None, err
 
-    def get_target_languages (self, context: Context, target_language_code) -> Tuple[ list, None ]:
+    def get_target_languages (self, context: Context, target_language_code) -> Tuple[ Union[list, None], None ]:
         """
-        This function returns the target languages for translation except the source (default) language
+        This function returns the target languages for translation except the source (default) language.
+        It calls the SupportedLanguageStore.list_supported_languages method to get all supported languages and then filters out the source language.
 
         Args:
         - context: Context
@@ -58,8 +64,16 @@ class TranslationsCacheService:
         - target_languages: list
         """
         supported_languages, err = SupportedLanguageStore().list_supported_languages(context, { })
-        return [ (to_third_party_lang_code(language.code), language.code) for language in supported_languages if
-                 language.code != DEFAULT_SOURCE_LANGUAGE_CODE ] if supported_languages else None, err
+        target_languages = []
+
+        if err:
+            return None, err
+
+        for language in supported_languages:
+            if language.code != DEFAULT_SOURCE_LANGUAGE_CODE:
+                target_languages.append(language)
+
+        return target_languages, err
 
     def translate_text_field (self, context: Context, field_value: str, target_language_code) -> Tuple[dict or None, None ]:
 
@@ -78,7 +92,7 @@ class TranslationsCacheService:
                                                                  source_language = target_language_tuple[ 0 ],
                                                                  target_language = source_language_code
                                                                  )
-                translation, err = self.create_translation(context, {
+                translation, err = self.create_translation(args={
                     "hash": text_hash,
                     "source_language": DEFAULT_SOURCE_LANGUAGE_CODE,
                     "target_language": target_language_tuple[1],
@@ -184,32 +198,31 @@ class TranslationsCacheService:
 
         return all_records
 
-    def translate_all_models (self, context: Context, language_code: str) -> Tuple[ dict or None, None ]:
+    def translate_all_models (self, language_code: str) -> Tuple[ dict or None, None ]:
         try:
             # Go through all the records of all models and return a list of all records [{:dict}] to translate
             all_records= self.create_list_of_all_records_to_translate(apps.get_models())
 
-            source_langauge = to_third_party_lang_code(DEFAULT_SOURCE_LANGUAGE_CODE)
+            source_language = to_third_party_lang_code(DEFAULT_SOURCE_LANGUAGE_CODE)
             target_language = to_third_party_lang_code(language_code)
 
             json_translator = JsonTranslator(all_records)
-            _, translated_text_entries, text_hashes = json_translator.translate(source_langauge, target_language)
+            _, translations, hashes = json_translator.translate(source_language,target_language)
 
-            for i in range(0, len(translated_text_entries)):
-                translation, err = self.create_translation(context, {
-                    "hash": text_hashes[i],
+            for i in range(0, len(translations)):
+                translation, err = self.create_translation( {
+                    "hash": hashes[i],
                     "source_language": DEFAULT_SOURCE_LANGUAGE_CODE,
                     "target_language": language_code,
-                    "translated_text": translated_text_entries[i]
+                    "translated_text": translations[i]
                 })
 
                 if err:
-                    log.error(f"Error caching translation for {text_hashes[i]}: {err}")
+                    log.error(f"Error caching translation for {hashes[i]}: {err}")
                     return None, err
-
 
             return { "message": "All models translation successful", }, None
 
         except Exception as e:
             log.exception(e)
-            return CustomMassenergizeError(str(e)), None
+            return e, None
