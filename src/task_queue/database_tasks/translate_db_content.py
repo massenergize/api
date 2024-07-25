7e9591064478d0e8dccfe09695ce2e7af49df2e6
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-07-25 12:04:56 +0000

    refactor: streamline translation task with reusable utils
    Refactored `load_db_contents_and_translate` method to use new utility functions `create_list_of_all_records_to_translate` and `filter_active_records`, reducing code duplication and improving readability.

--- src/task_queue/database_tasks/translate_db_content.py
+++ src/task_queue/database_tasks/translate_db_content.py
@@ -1,9 +1,9 @@
 from django.apps import apps
 from django.forms import model_to_dict
 from django.utils import timezone
-
 from _main_.utils.massenergize_logger import log
 from _main_.utils.translation import JsonTranslator
+from _main_.utils.utils import create_list_of_all_records_to_translate, filter_active_records
 from database.models import SupportedLanguage, TranslationsCache
 
 SOURCE_LANGUAGE_CODE = 'en'
@@ -40,35 +40,10 @@ class TranslateDBContents:
 			log.exception(e, extra={'MESSAGE_ID': 'TRANSLATION_ERROR'})
 			return False
 	
-	def get_valid_instances(self, model) -> list:
-		query_params = {}
-		if hasattr(model, 'is_deleted'):
-			query_params['is_deleted'] = False
-		if hasattr(model, 'is_archived'):
-			query_params['is_archived'] = False
-		if hasattr(model, 'is_published'):
-			query_params['is_published'] = True
-		if hasattr(model, 'is_active'):
-			query_params['is_active'] = True
-		return model.objects.filter(**query_params)
-	
 	def load_db_contents_and_translate(self) -> bool:
 		try:
 			models = apps.get_models()
-			data = []
-			for model in models:
-				valid_instances = self.get_valid_instances(model)
-				if not valid_instances:
-					continue
-				
-				if not hasattr(model, 'TranslationMeta'):
-					continue
-				
-				for instance in valid_instances:
-					to_dict = model_to_dict(instance, fields=instance.TranslationMeta.fields_to_translate)
-					if to_dict != {}:
-						data.append(to_dict)
-			
+			data = create_list_of_all_records_to_translate(models)
 			for lang in self.supported_languages:
 				log.info(f"Task: Translating DB contents to {lang}")
 				_json, translated_text_list, hashes = self.translator(data).translate("en", lang)
@@ -83,7 +58,7 @@ class TranslateDBContents:
 	def start_translations(self) -> bool:
 		try:
 			start_time = timezone.now()
-			log.info("Starting translation process for {}".format(timezone.now()))
+			log.info("Starting translation process for {}".format(start_time))
 			self.load_db_contents_and_translate()
 			log.info("Translation process took: {}".format(timezone.now() - start_time))
 			return True
