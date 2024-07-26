ee928d2e9d2c15c5fdca853452b2b2bb24371d6b
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-07-26 08:54:05 +0000

    test: fix import paths in test_translate_db_contents.py
    Updated mocked import paths in test_translate_db_contents.py for consistency and accuracy. Removed unused MagicMock import.

--- src/api/tests/unit/tasks/test_translate_db_contents.py
+++ src/api/tests/unit/tasks/test_translate_db_contents.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock, patch, Mock
+from unittest.mock import patch, Mock
 from database.models import Role, SupportedLanguage
 from task_queue.database_tasks.translate_db_content import TranslateDBContents
 
@@ -9,8 +9,8 @@ class TestTranslateDBContents(unittest.TestCase):
 		self.translate_db_contents = TranslateDBContents()
 		SupportedLanguage.objects.get_or_create(code='es', name='Spanish Spain')
 	
-	@patch('task_queue.database_tasks.translate_contents.apps')
-	@patch('task_queue.database_tasks.translate_contents.JsonTranslator')
+	@patch('task_queue.database_tasks.translate_db_contents.apps')
+	@patch('task_queue.database_tasks.translate_db_contents.JsonTranslator')
 	def test_load_db_contents_and_translate(self, mock_apps, mock_json_translator):
 		mock_model = Role
 		mock_apps.get_models.return_value = [mock_model]
@@ -18,7 +18,7 @@ class TestTranslateDBContents(unittest.TestCase):
 		result = self.translate_db_contents.load_db_contents_and_translate()
 		self.assertTrue(result)
 	
-	@patch('task_queue.database_tasks.translate_contents.JsonTranslator')
+	@patch('task_queue.database_tasks.translate_db_contents.JsonTranslator')
 	def test_start_translations(self, mock_json_translator):
 		mock_json_translator.return_value = Mock(translate=Mock(return_value=True))
 		result = self.translate_db_contents.start_translations()
