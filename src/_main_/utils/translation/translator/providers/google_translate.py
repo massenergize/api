3128c930363062d4d3a4ec1dc8f6e09b6ab39d0b
Kobina Koomson <georgeranch31@gmail.com>
2024-08-03 11:08:52 +0000

    Add Translator utils (#1120)
    This PR introduces Translator utils and suite of enhancements to our translation capabilities. It includes the implementation of translation utilities with Google and Microsoft services, the introduction of a languages.json file for supported languages, and refined JSON translation management using regex patterns and a TextHashService.

---
+++ src/_main_/utils/translation/translator/providers/google_translate.py
@@ -0,0 +1,18 @@
+import os
+from google.cloud import translate_v2 as translate
+from google.oauth2 import service_account
+
+from _main_.settings import GOOGLE_TRANSLATE_CLIENT
+
+
+class GoogleTranslate:
+    MAX_TEXT_SIZE = 5000
+
+    def __init__(self):
+        self.__client: translate.Client = GOOGLE_TRANSLATE_CLIENT
+        self.__name__ = "GoogleTranslate"
+
+    def translate(self, text, source_language_code, target_language_code):
+        # TODO: we need to support lists of texts
+        result = self.__client.translate(text, source_language=source_language_code, target_language=target_language_code)
+        return result['translatedText']
