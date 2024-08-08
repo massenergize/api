0452535f4a13f8a2b56eed95e3fb0015f511c376
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-08-08 22:12:38 +0000

    refactor: remove unused cache_translations method in translate_db_content.py
    Cleaned up code by removing the unused cache_translations method and associated imports in translate_db_content.py to streamline translation process.

--- src/task_queue/database_tasks/translate_db_content.py
+++ src/task_queue/database_tasks/translate_db_content.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 from _main_.utils.massenergize_logger import log
 from _main_.utils.metrics import timed
 from _main_.utils.translation import JsonTranslator
-from _main_.utils.utils import create_list_of_all_records_to_translate, make_hash, to_third_party_lang_code
-from database.models import SupportedLanguage, TranslationsCache
+from _main_.utils.utils import create_list_of_all_records_to_translate, to_third_party_lang_code
+from database.models import SupportedLanguage
 
 SOURCE_LANGUAGE_CODE = 'en'
 
@@ -28,29 +28,8 @@ class TranslateDBContents:
 			_supported_languages = SupportedLanguage.objects.values_list('code', flat=True)
 			return [to_third_party_lang_code(lang_code) for lang_code in _supported_languages]
 		except Exception as e:
-			print("==ERROR GETTING SUPPORTED LANGUAGES: ", e)
 			log.exception(e)
 			return []
-	
-	@timed
-	def cache_translations(self, raw_texts, translated_text_list, language) -> bool:
-		try:
-			caches = []
-			for text, translated_text in zip(raw_texts, translated_text_list):
-				_hash = make_hash(text)
-				cache = TranslationsCache(
-					hash=_hash,
-					target_language_code=language,
-					source_language_code=SOURCE_LANGUAGE_CODE,
-					translated_text=translated_text
-				)
-				caches.append(cache)
-			TranslationsCache.objects.bulk_create(caches, batch_size=500)
-			return True
-		except Exception as e:
-			print("==ERROR CACHING TRANSLATIONS: ", e)
-			log.exception(e)
-			return False
 	
 	def load_db_contents_and_translate(self) -> bool:
 		try:
@@ -60,13 +39,11 @@ class TranslateDBContents:
 			supported_languages = self.get_supported_languages()
 			for lang in supported_languages:
 				log.info(f"Task: Translating DB contents to {lang}")
-				_json, translated_text_list, raw_texts = self.translator({"data":data}).translate("en", lang)
-				self.cache_translations(raw_texts, translated_text_list, lang)
-			
+				self.translator({"data": data}).translate("en", lang)
+				
 			log.info("Task: Finished translating all DB contents")
 			return True
 		except Exception as e:
-			print("==ERROR HERE: ", e)
 			log.exception(e)
 			return False
 	
