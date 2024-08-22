3bf574436c376bf0aab24496cf15946a6bdb361b
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-08-22 13:38:40 +0000

    ✨ chore(database): simplify recurring event scheduling
    Refactored to remove unnecessary logging and streamline imports.
    
    Benefits:
    - Cleaner code
    - Improved readability

--- src/task_queue/database_tasks/create_initial_schedule_for_recurring_events.py
+++ src/task_queue/database_tasks/create_initial_schedule_for_recurring_events.py
@@ -1,12 +1,10 @@
-from _main_.utils.massenergize_logger import log
-from api.tasks import update_recurring_event
 from database.models import Event
+from task_queue.database_tasks.update_recurring_events import update_recurring_event
 
 
 def create_initial_schedule():
     events = Event.objects.filter(is_recurring=True, is_deleted=False, recurring_details__isnull=False)
     
     for event in events:
-        log.info(f"==== Initial Schedule for: {event.name}======")
-        update_recurring_event(str(event.id))
-        log.info(f"==== DONE: for {event.name}====")
+        update_recurring_event(event)
+        
