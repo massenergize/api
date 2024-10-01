506aee2caab942894905cf58b35b993f7c77f2ed
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-10-01 09:28:40 +0000

    ✨ feat(api): add optional call_to_action field in technology handler
    This enhances flexibility by allowing an optional call_to_action.

--- src/api/handlers/technology.py
+++ src/api/handlers/technology.py
@@ -116,6 +116,7 @@ class TechnologyHandler(RouteHandler):
         self.validator.expect("vendors_section", dict, is_required=False)
         self.validator.expect("more_info_section", dict, is_required=False)
         self.validator.expect("faq_section", dict, is_required=False)
+        self.validator.expect("call_to_action", dict, is_required=False)
 
         args, err = self.validator.verify(args, strict=True)
 
