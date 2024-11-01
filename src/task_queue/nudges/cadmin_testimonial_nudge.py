f5e2bc2117a7373c870169861dcf0cf13abf2a86
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-11-01 11:11:09 +0000

    refactor: Update cadmin_testimonial_nudge.py and nudge_utils.py
    - Update cadmin_testimonial_nudge.py to include additional imports and variables for sending nudges to community admins.
    - Add a new function, get_admin_testimonials, to filter testimonials based on notification dates and send them to community admins.
    - Modify prepare_testimonials_for_community_admins to use the new function and update last notification dates for admins.
    - Update nudge_utils.py to include helper functions get_admin_email_list and update_last_notification_dates.

--- src/task_queue/nudges/cadmin_testimonial_nudge.py
+++ src/task_queue/nudges/cadmin_testimonial_nudge.py
@@ -1,14 +1,20 @@
-from _main_.utils.common import encode_data_for_URL, parse_datetime_to_aware, serialize_all
+from datetime import datetime
+from _main_.utils.common import encode_data_for_URL, serialize_all
 from _main_.utils.constants import ADMIN_URL_ROOT, COMMUNITY_URL_ROOT
 from _main_.utils.emailer.send_email import send_massenergize_email_with_attachments
 from _main_.utils.feature_flag_keys import SHARED_TESTIMONIALS_NUDGE_FF
 from _main_.utils.massenergize_logger import log
 from api.utils.api_utils import get_sender_email
 from api.utils.constants import CADMIN_TESTIMONIAL_NUDGE_TEMPLATE
-from database.models import Community, CommunityAdminGroup, FeatureFlag
-from database.utils.common import get_json_if_not_none
+from database.models import Community, CommunityAdminGroup, FeatureFlag, Testimonial
 from task_queue.helpers import get_summary
-from task_queue.nudges.nudge_utils import ME_DEFAULT_IMAGE
+from task_queue.nudges.nudge_utils import ME_DEFAULT_IMAGE, get_admin_email_list, update_last_notification_dates
+from dateutil.relativedelta import relativedelta
+from django.db.models import Q
+from django.utils import timezone
+
+
+TESTIMONIAL_NUDGE_KEY = "cadmin_testimonial_nudge"
 	
 def get_cadmin_names_and_emails(community):
 	"""
@@ -17,31 +23,42 @@ def get_cadmin_names_and_emails(community):
 	name_emails = {}
 	try:
 		c = (CommunityAdminGroup.objects.filter(community=community)
-			.values_list('members__full_name', 'members__email', "members__user_info", 'members__preferences',  "members__notification_dates")
+			.values_list(
+				'members__full_name',
+				'members__email', 
+				"members__user_info", 
+				'members__preferences',  
+				"members__notification_dates"
+			)
 		)
 		
-		for (name, email, user_info, preference, nudge_dates) in list(c):
+		for (name, email, user_info, preferences, nudge_dates) in list(c):
 			name_emails[email] = {
 				"name": name,
-				"email":email,
-				"preference": preference,
+				"preferences": preferences,
 				"nudge_dates": nudge_dates,
 				"user_info": user_info
 			}
 			
 	except Exception as e:
-		log.exception(str(e))
+		print("Failed to get community admins", str(e))
+		log.exception(e)
 		return None
 		
 	return name_emails
+
+
+
 	
-def send_nudge(data, community):
+def send_nudge(data, community, admin):
 	"""
 		Send a nudge to the community admin to approve testimonials
 	"""
-	try:
-		admins = get_cadmin_names_and_emails(community)
-		
+	try:		
+		email = admin.get("email")
+		name = admin.get("name")
+		user_info = admin.get("user_info")
+
 		serialized_data = serialize_all(data)
 		to_send = [
 			{
@@ -51,36 +68,52 @@ def send_nudge(data, community):
 				"community": testimonial.get("community", {}).get("name"),
 				"view_url": f"{COMMUNITY_URL_ROOT}/{community.subdomain}/testimonials/{testimonial.get('id')}",
 				"date": testimonial.get('published_at').strftime("%B %d, %Y"),
-				"image_url": testimonial.get('image', {}).get('url', None)
+				"image": {"url": testimonial.get('image', {}).get('url', None)} if testimonial.get('image') else None,
 			} for testimonial in serialized_data
 		]
 		data = {
 			"testimonials": to_send,
 			"community_name": community.name,
-			"community_logo":get_json_if_not_none(community.logo).get("url") if community.logo else ME_DEFAULT_IMAGE
+			"community_logo": community.logo.file.url if community.logo else ME_DEFAULT_IMAGE,
+			"name": name,
 		}
+		login_method = user_info.get("login_method") if user_info else None
+		cred = encode_data_for_URL({"email": email, "login_method": login_method})
+		data["change_preference_link"] = f"{ADMIN_URL_ROOT}/admin/profile/preferences/?cred={cred}"
+		 
+		send_massenergize_email_with_attachments(CADMIN_TESTIMONIAL_NUDGE_TEMPLATE, data, [email], None, None, get_sender_email(community.id))
 
-		
-		from_email = get_sender_email(community.id)
-		
-		for email, (name, user_info) in admins.items():
-			data["name"] = name
-			login_method = user_info.get("login_method") if user_info else None
-			cred = encode_data_for_URL({"email": email, "login_method": login_method})
-			data["change_preference_link"] = f"{ADMIN_URL_ROOT}/admin/profile/preferences/?cred={cred}"
-			send_massenergize_email_with_attachments(CADMIN_TESTIMONIAL_NUDGE_TEMPLATE, data, [email], None, None, from_email)
-		
+					
 		return True
 	except Exception as e:
-		log.exception(str(e))
+		print("Failed to send nudge to community admin", str(e))
+		log.exception(e)
 		return False
+	
+
+
+def get_admin_testimonials(notification_dates, testimonial_list):
+	today = timezone.now()
+	a_week_ago = today - relativedelta(weeks=4)
+
+	user_event_nudge = notification_dates.get(TESTIMONIAL_NUDGE_KEY, None)
+
+	if not user_event_nudge or not notification_dates:
+		return testimonial_list.filter(Q(published_at__range=[a_week_ago, today])).order_by('published_at')
+	
+	last_received_at = datetime.strptime(user_event_nudge, '%Y-%m-%d')
+	date_aware = timezone.make_aware(last_received_at, timezone=timezone.get_default_timezone())
+
+	last_time = date_aware if date_aware else a_week_ago
+
+	return testimonial_list.filter(Q(published_at__range=[last_time, today])).order_by('published_at')
+
 	
 def prepare_testimonials_for_community_admins(task=None):
 	"""
 	Prepare a list of testimonials that are pending approval for each community admin
 	"""
 	try:
-		today = parse_datetime_to_aware() 
         
 		flag = FeatureFlag.objects.get(key=SHARED_TESTIMONIALS_NUDGE_FF)
 		if not flag or not flag.enabled():
@@ -89,25 +122,41 @@ def prepare_testimonials_for_community_admins(task=None):
 		communities = Community.objects.filter(is_published=True, is_deleted=False)
 		communities = flag.enabled_communities(communities)
 
-
-		# for each get admin and amin communication  pref
-		# get ll shared testimonials for that community
-		# if there are no shared testimonials, skip
-		# else send nudge to admin
-
 		for community in communities:
-			testimonials_auto_shared = community.testimonial_shares.filter(testimonial__is_published=True, testimonial__published_at__date=today.date())
+			testimonials_auto_shared = community.testimonial_shares.filter(testimonial__is_published=True)
+
 			if not testimonials_auto_shared:
 				continue
 			
 			list_to_send = [t.testimonial for t in testimonials_auto_shared]
-			
-			ok = send_nudge(list_to_send, community)
-			if not ok:
-				log.error(f"Failed to send nudge to community admin for community {community.name}")
+
+			testimonials = Testimonial.objects.filter(id__in=[t.id for t in list_to_send]).order_by('published_at')
+		
+			all_admins = get_cadmin_names_and_emails(community)
+
+			if not all_admins:
+				log.info(f"No community admins found for community {community.name}")
+				continue
+
+			admins_to_receive = get_admin_email_list(all_admins, TESTIMONIAL_NUDGE_KEY)
+
+			for email, data in admins_to_receive.items():
+				name = data.get("name")
+				nudge_dates = data.get("nudge_dates")
+				user_info = data.get("user_info")
+				admin_testimonials = get_admin_testimonials(nudge_dates, testimonials)
+				if not admin_testimonials:
+					continue
+		
+				ok = send_nudge(admin_testimonials, community, {"name": name, "email": email, "user_info": user_info})
+				if not ok:
+					log.error(f"Failed to send nudge to community admin for community {community.name}")
+
+			update_last_notification_dates(admins_to_receive.keys(), TESTIMONIAL_NUDGE_KEY)
 
 		log.info("Successfully sent nudge to all community admins")
 		return True
 	except Exception as e:
-		log.exception(str(e))
+		print("Failed to send nudge to community admins", str(e))
+		log.exception(e)
 		return False
--- src/task_queue/nudges/nudge_utils.py
+++ src/task_queue/nudges/nudge_utils.py
@@ -1,4 +1,10 @@
 from _main_.utils.common import custom_timezone_info
+from database.models import UserProfile
+from database.utils.settings.admin_settings import AdminPortalSettings
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
+
 
 
 WEEKLY = "per_week"
@@ -33,3 +39,56 @@ DEFAULT_EVENT_SETTINGS = {
     "within_1_week": True,
     "never": False,
 }
+
+# ------------------ Helper Functions ------------------ #
+def get_admin_email_list(admins, nudge_type) -> dict:
+    """
+    Get the list of admin emails to send the nudge to. 
+    """
+    email_list = {}
+    frequency_map = {
+        'WEEKLY': relativedelta(weeks=1),
+        'BI_WEEKLY': relativedelta(weeks=2),
+        'MONTHLY': relativedelta(months=1)
+    }
+
+    for email, data in admins.items():
+        name = data.get("name")
+        preferences = data.get("preferences")
+        nudge_dates = data.get("nudge_dates")
+        user_info = data.get("user_info")
+
+        if not name or not email:
+            print(f"Missing name or email for admin: {name}")
+            continue
+
+        admin_preferences = preferences or AdminPortalSettings.Defaults
+        admin_settings = admin_preferences.get("communication_prefs", {})
+        freq = admin_settings.get("reports_frequency", {})
+
+        notification_dates = nudge_dates
+        last_notified = notification_dates.get(nudge_type, "") if notification_dates else None
+
+        if last_notified:
+            last_notified_date = datetime.strptime(last_notified, '%Y-%m-%d').date()
+            for freq_key, delta in frequency_map.items():
+                if freq_key in freq.keys() or len(freq.keys()) == 0:
+                    next_nudge_date = last_notified_date + delta
+                    if next_nudge_date <= date.today():
+                        email_list[name] = email
+                        break
+        else:
+            email_list[email] = {"name": name, "user_info": user_info, "nudge_dates": nudge_dates}
+
+    return email_list
+
+
+def update_last_notification_dates(emails, nudge_type):
+    new_date = str(timezone.now())
+    all_users = UserProfile.objects.filter(email__in=emails)
+    for email in emails:
+        users = all_users.filter(email=email)
+        user = users.first()
+        user_notification_dates = user.notification_dates if user.notification_dates else {}
+        notification_dates = {**user_notification_dates, [nudge_type]:new_date}
+        user.update(**{"notification_dates":notification_dates })
