affaf3d407d96a771c3828ef0d2c7b606cf26b5f
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2025-01-31 09:58:44 +0000

    url resolve

--- src/api/middlewares/url_resolver_middleware.py
+++ src/api/middlewares/url_resolver_middleware.py
@@ -4,7 +4,8 @@ from _main_.hosts import host_patterns
 class URLResolverMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.host_map = {host.regex: host for host in host_patterns}
+        # Store hosts directly instead of regex patterns
+        self.host_patterns = host_patterns
 
     def __call__(self, request):
         _path = self._resolve_path(request)
@@ -17,15 +18,24 @@ class URLResolverMiddleware:
     def _resolve_path(self, request):
         try:
             current_host = request.get_host().split('.')[0]
-            if current_host in self.host_map:
-               if request.path == '/':
+            
+            # Check if the current host matches any of our patterns
+            matching_host = None
+            for host in self.host_patterns:
+                if str(host.regex).strip('^$') == current_host:
+                    matching_host = host
+                    break
+            
+            if matching_host:
+                if request.path == '/':
                     _path = f'/{current_host}'
-               else:
+                else:
                     _path = f'/{current_host}{request.path}'
             else:
                 _path = request.path
 
             resolve(_path)  # Check if path is valid
             return _path
-        except Resolver404:
+        except Resolver404 as e:
+            print("===ERROR===", str(e))
             return None
