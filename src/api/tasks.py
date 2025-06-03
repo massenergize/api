ab1491f8d044da3a260fb538f4afad0f30c97361
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2025-06-03 12:19:22 +0000

    Enhance data export functionality in download service
    * Added new export options for actions, testimonials, events, CC actions, and vendors to the download_data method.
    * Implemented error handling and notification for each export type to improve user experience and data integrity.
    * Updated constants to include new export types for better organization and clarity.

--- src/api/tasks.py
+++ src/api/tasks.py
@@ -13,7 +13,7 @@ from _main_.utils.context import Context
 from _main_.utils.emailer.send_email import send_massenergize_email, send_massenergize_email_with_attachments
 from _main_.utils.massenergize_logger import log
 from api.constants import ACTIONS, ACTION_USERS, ACTIONS_USERS, CADMIN_REPORT, CAMPAIGN_INTERACTION_PERFORMANCE_REPORT, CAMPAIGN_PERFORMANCE_REPORT, \
-    CAMPAIGN_VIEWS_PERFORMANCE_REPORT, COMMUNITIES, COMMUNITY_PAGEMAP, DOWNLOAD_POLICY, FOLLOWED_REPORT, LIKE_REPORT, \
+    CAMPAIGN_VIEWS_PERFORMANCE_REPORT, COMMUNITIES, COMMUNITY_PAGEMAP, DOWNLOAD_POLICY, EXPORT_ACTIONS, EXPORT_CC_ACTIONS, EXPORT_EVENTS, EXPORT_TESTIMONIALS, EXPORT_VENDORS, FOLLOWED_REPORT, LIKE_REPORT, \
     LINK_PERFORMANCE_REPORT, METRICS, POSTMARK_NUDGE_REPORT, SAMPLE_USER_REPORT, TEAMS, USERS
 from api.services.translations_cache import TranslationsCacheService
 from api.store.common import create_pdf_from_rich_text, sign_mou
@@ -233,6 +233,46 @@ def download_data(self, args, download_type):
             generate_csv_and_email(data=files, download_type=CAMPAIGN_INTERACTION_PERFORMANCE_REPORT, email=email)
         
        
+
+           # data export
+    elif download_type == EXPORT_ACTIONS:
+        (files, com_name), err = store.export_actions(context, community_id=args.get("community_id"))
+
+        if err:
+            error_notification(EXPORT_ACTIONS, email)
+        else:
+            generate_csv_and_email(data=files, download_type=EXPORT_ACTIONS, community_name=com_name, email=email)
+
+    elif download_type == EXPORT_TESTIMONIALS:
+        (files, com_name), err = store.export_testimonials(context, community_id=args.get("community_id"))
+
+        if err:
+            error_notification(EXPORT_TESTIMONIALS, email)
+        else:
+            generate_csv_and_email(data=files, download_type=EXPORT_TESTIMONIALS, community_name=com_name, email=email)
+
+    elif download_type == EXPORT_EVENTS:
+        (files, com_name), err = store.export_events(context, community_id=args.get("community_id"))
+        if err:
+            error_notification(EXPORT_EVENTS, email)
+        else:
+            generate_csv_and_email(data=files, download_type=EXPORT_EVENTS, community_name=com_name, email=email)
+
+
+    elif download_type == EXPORT_CC_ACTIONS:
+        (files, com_name), err = store.export_cc_actions(context, community_id=args.get("community_id"))
+        if err:
+            error_notification(EXPORT_CC_ACTIONS, email)
+        else:
+            generate_csv_and_email(data=files, download_type=EXPORT_CC_ACTIONS, community_name=com_name, email=email)
+
+
+    elif download_type == EXPORT_VENDORS:
+        (files, com_name), err = store.export_vendors(context, community_id=args.get("community_id"))
+        if err:
+            error_notification(EXPORT_VENDORS, email)
+        else:
+            generate_csv_and_email(data=files, download_type=EXPORT_VENDORS, community_name=com_name, email=email)
 
 
 
